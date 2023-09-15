28f9e831780f872872750dec92ea1134393d208e
yashladd <43058610+yashladd@users.noreply.github.com>
2023-09-15 12:11:16 -0600

    Time: 39 ms (73.91%), Space: 16.1 MB (99.10%) - LeetHub

---
+++ 0289-game-of-life/0289-game-of-life.py
@@ -0,0 +1,38 @@
+class Solution:
+    def gameOfLife(self, board: List[List[int]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+
+
+        n, m = len(board), len(board[0])
+
+
+        def countOneNeighs(r, c):
+            nei = 0
+            for i in range(r-1, r+2):
+                for j in range(c-1, c+2):
+                    if (i == r and j == c) or i < 0 or j < 0 or i >=n or j >= m:
+                        continue
+                    if board[i][j] in [1, 3]:
+                        nei += 1
+            return nei
+
+        for i in range(n):
+            for j in range(m):
+                neighs = countOneNeighs(i, j)
+
+                if board[i][j]:
+                    if neighs in [2, 3]:
+                        board[i][j] = 3
+                else:
+                    if neighs == 3:
+                        board[i][j] = 2
+
+        for i in range(n):
+            for j in range(m):
+                if board[i][j] in [2, 3]:
+                    board[i][j] = 1
+                elif board[i][j] == 1:
+                    board[i][j] = 0
+                
