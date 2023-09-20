26567b6724fea8e97b4e18dd0966e2dfa80019a0
yashladd <43058610+yashladd@users.noreply.github.com>
2023-09-20 17:16:56 -0600

    Time: 39 ms (58.44%), Space: 16.1 MB (91.02%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -3,20 +3,11 @@ class Solution:
         n = len(nums)
         if n == 1:
             return nums[0]
-        dp = [0] * n
-        dp[0] = nums[0]
-        dp[1] = max(nums[0], nums[1])
+        prev = nums[0]
+        prev2 = max(nums[0], nums[1])
         for i in range(2, n):
-            dp[i] = max(dp[i-1], nums[i] + dp[i-2])
-            
-        return dp[n-1]
-        def f(n):
-            if n < 0:
-                return 0
-            if dp[n] != -1:
-                return dp[n]
-            take = nums[n] + f(n-2)
-            notTake = f(n-1)
-            dp[n] = max(take, notTake)
-            return max(take, notTake)
-        return f(len(nums)-1)
+            curr = max(prev2, nums[i] + prev)
+            prev = prev2
+            prev2 = curr
+            # print(prev, prev2, curr)
+        return prev2
