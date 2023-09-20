f29596025a11235bee099748d9b3b6d9e6b08616
yashladd <43058610+yashladd@users.noreply.github.com>
2023-09-20 17:06:46 -0600

    Time: 40 ms (45.70%), Space: 16.3 MB (61.53%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,23 +1,15 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # dp = [-1] * (n + 1)
-        # def f(n):
-        #     nonlocal dp
-        #     if n in (0, 1, 2):
-        #         dp[n] = n
-        #         return n
-        #     if dp[n] != -1:
-        #         return dp[n]
-        #     dp[n] =  f(n-1) + f(n-2)
-        #     return dp[n]
-        # return f(n)
         if n <= 2:
             return n
         dp = [0] * (n + 1)
         dp[1] = 1
         dp[2] = 2
+        prev, prev2 = 1, 2
         for i in range(3, n+1):
-            dp[i] = dp[ i-1] + dp[ i-2]
-        return dp[n]
+            curr = prev + prev2
+            prev = prev2
+            prev2 = curr
+        return prev2
             
             
