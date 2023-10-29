c835ebdb216b1eed5c8858a381c10dcfcced46b6
yashladd <43058610+yashladd@users.noreply.github.com>
2023-10-29 10:13:40 -0600

    Time: 137 ms (60.57%), Space: 18.2 MB (51.82%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -4,30 +4,30 @@ class Solution:
         Do not return anything, modify board in-place instead.
         """
         n, m = len(board), len(board[0])
-        vis = [[0 for _ in range(m)] for _ in range(n)]
-
-        def inb(i, j):
-            return i >= 0 and j >= 0 and i < n and j < m
+        if not n: return 
+        
         def dfs(i, j):
-            vis[i][j] = 1
-            for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
-                r, c = i + dx, j + dy
-                if inb(r, c) and board[r][c] == "O" and not vis[r][c]:
-                    dfs(r, c)
-
-        for c in range(m):
-            if not vis[0][c] and board[0][c] == "O":
-                dfs(0, c)
-            if not vis[n-1][c] and board[n-1][c] == "O":
-                dfs(n-1, c)
-
-        for r in range(1, n-1):
-            if not vis[r][0] and board[r][0] == "O":
-                dfs(r, 0)
-            if not vis[r][m-1] and board[r][m-1] == "O":
-                dfs(r, m-1)
-
+            if i < 0 or j < 0 or i >= n or j >= m \
+                or board[i][j] != "O":
+                return 
+            
+            board[i][j] = "T"
+            for dx, dy in [(0,1), (1,0), (-1,0), (0,-1)]:
+                x, y = i + dx, j + dy
+                dfs(x, y)
+                
+        for i in range(n):
+            dfs(i, 0)
+            dfs(i, m-1)
+            
+        for j in range(1, m):
+            dfs(0, j)
+            dfs(n-1, j)
+            
         for i in range(n):
             for j in range(m):
-                if board[i][j] == "O" and not vis[i][j]:
-                    board[i][j] = "X" 
+                if board[i][j] == "T":
+                    board[i][j] = "O"
+                else:
+                    board[i][j] = "X"
+        
