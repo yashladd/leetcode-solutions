5f2d27ac3160c227de5cc304961f8856bd643299
yashladd <43058610+yashladd@users.noreply.github.com>
2023-12-15 16:00:07 -0700

    Time: 101 ms (99.57%), Space: 17.5 MB (32.22%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,19 +1,17 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        n = len(nums)
-        @cache
-        def f(i):
-            if i == n-1:
-                return 0
+        j = 0
+        l, r = 0, 0
+        while r < len(nums)-1:
+            j += 1
+            maxi = l + nums[l]
+            for i in range(l+1, r+1):
+                maxi = max(maxi, i + nums[i])
+            l = r + 1
+            r = maxi
             
-            if i < n-1 and i + nums[i] >= n-1:
-                return 1
-            
-            mini = 1 + f(i + 1)
-            for j in range(i+2, i + nums[i] + 1):
-                mini = min(1 + f(j), mini)
-
-            return mini
-        
-        return f(0)
-        
+        return j
+    
+    
+    
+    
