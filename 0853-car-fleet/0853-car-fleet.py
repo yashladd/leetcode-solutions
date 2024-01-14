154e9a67815fcd5f0662b49e1a93fd700f2463b8
yashladd <43058610+yashladd@users.noreply.github.com>
2024-01-14 13:26:54 -0700

    Time: 653 ms (97.39%), Space: 39.6 MB (29.66%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -1,13 +1,12 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        a = [(p, s) for p, s in zip(position, speed)]
+        a = [(target - p) / s for p, s in sorted(zip(position, speed))]
         
         stk = []
-        
-        for p, s in sorted(a, reverse=True):
-            time = (target - p) / s
-            stk.append(time)
-            if len(stk) >= 2 and stk[-1] <= stk[-2]:
-                stk.pop()
+        res = curr = 0
+        for t in a[::-1]:
+            if t > curr:
+                res += 1
+                curr = t
                 
-        return len(stk)
+        return res
