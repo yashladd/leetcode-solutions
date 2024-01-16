a27c10447d493716c93f1503722db8d908e90f78
yashladd <43058610+yashladd@users.noreply.github.com>
2024-01-15 22:54:10 -0700

    Time: 44 ms (43.66%), Space: 18.3 MB (8.01%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -9,22 +9,36 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        dummy = Node(-1)
-        h = {}
-        h[None] = None
-        tail = dummy
-        curr = head
-        while curr:
-            copy = Node(curr.val)
-            h[curr] = copy
-            tail.next = copy
-            tail = copy
-            curr = curr.next
-            
-        curr = head
-        while curr:
-            rand = h[curr.random]
-            h[curr].random = rand
-            curr = curr.next
-            
-        return dummy.next
+        if not head: return None
+        
+        def copy():
+            c = head
+            while c:
+                nex = c.next
+                node = Node(c.val)
+                c.next = node
+                node.next = nex
+                c = nex
+                
+        def random():
+            c = head
+            while c:
+                if c.random:
+                    c.next.random = c.random.next
+                c = c.next.next
+        
+        def remove():
+            dummy = Node(-1, head.next)
+            tail = dummy
+            curr = head
+            while curr:
+                tail.next = curr.next
+                tail = tail.next
+                curr.next = tail.next
+                curr = curr.next
+            return dummy.next
+        
+        copy()
+        random()
+        return remove()
+        
