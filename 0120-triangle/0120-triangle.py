884d330dd5081d1db3f949656795b1a0386e1421
yashladd <43058610+yashladd@users.noreply.github.com>
2024-01-20 15:46:11 -0700

    Time: 69 ms (46.02%), Space: 17.8 MB (66.74%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,6 +1,16 @@
 class Solution:
     def minimumTotal(self, a: List[List[int]]) -> int:
         n = len(a)
+        dp = [[0] * (len(a[-1])) for _ in range(n)]
+        for j in range(len(dp[-1])):
+            dp[n-1][j] = a[n-1][j]
+            
+        for i in range(n-2, -1, -1):
+            for j in range(len(a[i])):
+                dp[i][j] = min(a[i][j] + dp[i+1][j], a[i][j] + dp[i+1][j+1])
+                
+        return dp[0][0]
+        
         @cache
         def f(i, j):
             if i == n-1:
