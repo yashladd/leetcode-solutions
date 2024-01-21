f32da0f4f5c347400db6708f613b9a4b0bd99849
yashladd <43058610+yashladd@users.noreply.github.com>
2024-01-20 20:49:12 -0700

    Time: 71 ms (90.91%), Space: 18.7 MB (75.57%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.py
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -5,7 +5,7 @@ class Solution:
         prev, secPrev = 0, 0
         for i in range(len(a)):
             currEarn = a[i] * h[a[i]]
-            if a[i] - a[i-1] == 1:
+            if i > 0 and a[i] - a[i-1] == 1:
                 tmp = prev
                 prev = max(currEarn + secPrev, prev)
                 secPrev = tmp
