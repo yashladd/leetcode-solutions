2c8ede04f121b179a54b2e33eff6f86bd0649f6a
yashladd <43058610+yashladd@users.noreply.github.com>
2024-01-20 20:48:28 -0700

    Time: 76 ms (77.72%), Space: 18.7 MB (75.57%) - LeetHub

---
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -0,0 +1,20 @@
+class Solution:
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        h = Counter(nums)
+        a = sorted(list(h.keys()))
+        prev, secPrev = 0, 0
+        for i in range(len(a)):
+            currEarn = a[i] * h[a[i]]
+            if a[i] - a[i-1] == 1:
+                tmp = prev
+                prev = max(currEarn + secPrev, prev)
+                secPrev = tmp
+            else:
+                tmp = prev
+                prev = currEarn + prev
+                secPrev = tmp
+                
+        return prev
+        
+            
+        
