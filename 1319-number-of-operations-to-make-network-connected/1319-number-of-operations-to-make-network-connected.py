8b3de15a87b5450a13215e56c39aa1a8b07ba656
yashladd <43058610+yashladd@users.noreply.github.com>
2024-01-23 16:36:45 -0700

    Time: 413 ms (61.85%), Space: 46.8 MB (14.94%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
@@ -1,48 +1,24 @@
-class Ds:
-    def __init__(self, n):
-        self.n = n
-        self.parent = [0] * (self.n + 1)
-        self.size = [1] * (self.n + 1)
-        for i in range(n+1):
-            self.parent[i] = i
-            
-    def findUParent(self, node):
-        if node == self.parent[node]:
-            return node
-        self.parent[node] = self.findUParent(self.parent[node])
-        return self.parent[node]
-
-    def unionBySize(self, u, v):
-        up_u, up_v = self.findUParent(u), self.findUParent(v)
-        if up_u == up_v:
-            return 
-        if self.size[up_u] < self.size[up_v]:
-            self.parent[up_u] = up_v
-            self.size[up_v] += self.size[up_u]
-        else:
-            self.parent[up_v] = up_u
-            self.size[up_u] += self.size[up_v]
-                
-            
-            
-
 class Solution:
     def makeConnected(self, n: int, connections: List[List[int]]) -> int:
-        ds = Ds(n)
-        extraEdges = 0
-        for u, v in connections:
-            if ds.findUParent(u) == ds.findUParent(v):
-                extraEdges += 1
-                continue
-            else:
-                ds.unionBySize(u, v)
+        if len(connections) < n-1: return -1
         
-        numComponents = 0
+        G = defaultdict(set)
+        for u, v in connections:
+            G[u].add(v)
+            G[v].add(u)
+            
+        vis = {}
+        def dfs(node):
+            vis[node] = 1
+            for nei in G[node]:
+                if nei not in vis:
+                    dfs(nei)
+                    
+        comp = 0
         for i in range(n):
-            if i == ds.parent[i]:
-                numComponents += 1
+            if i not in vis:
+                dfs(i)
+                comp += 1
                 
-        return numComponents - 1 if extraEdges >= numComponents - 1 else -1
-            
-            
+        return comp - 1
         
