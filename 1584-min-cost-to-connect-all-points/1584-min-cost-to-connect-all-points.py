765a4e5fd4dbdba0023cae1224babede940838b8
yashladd <43058610+yashladd@users.noreply.github.com>
2024-01-25 20:03:33 -0700

    Time: 7263 ms (5.00%), Space: 279.8 MB (5.03%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -0,0 +1,31 @@
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        n = len(points)
+        
+        G = defaultdict(list)
+        
+        for i in range(n):
+            for j in range(n):
+                if i != j:
+                    x1, y1 = points[i]
+                    x2, y2 = points[j]
+                    wt = abs(x1-x2) + abs(y1-y2)
+                    G[i].append((j, wt))
+                    G[j].append((i, wt))
+        
+        minCost = 0
+        vis = set()
+        
+        h = []
+        heappush(h, (0, 0))
+        
+        while h:
+            wt, node = heappop(h)
+            if node in vis: continue
+            vis.add(node)
+            minCost += wt
+            for nei, wt in G[node]:
+                if nei not in vis:
+                    heappush(h, (wt, nei))
+                    
+        return minCost
