eadee9fa76cbebb2fe7ba991119084ec31740b98
yashladd <43058610+yashladd@users.noreply.github.com>
2024-01-26 12:05:33 -0700

    Time: 688 ms (49.19%), Space: 20.2 MB (78.76%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.py
+++ 1462-course-schedule-iv/1462-course-schedule-iv.py
@@ -1,24 +1,34 @@
 class Solution:
     def checkIfPrerequisite(self, n: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
-        G = [[float("inf")] * n for _ in range(n)]
-        for i in range(n):
-            G[i][i] = 0
-            
+        g = defaultdict(list)
+        ind = [0] * n
         for u, v in prerequisites:
-            G[u][v] = 1
-            
-        for k in range(n):
-            for i in range(n):
-                for j in range(n):
-                    G[i][j] = min(G[i][j], G[i][k] + G[k][j])
-                    
-        ans = [False] * len(queries)
+            g[u].append(v)
+            ind[v] += 1
         
-        for i, (u, v) in enumerate(queries):
-            if G[u][v] != float("inf"):
-                ans[i] = True
+        q = deque([])
+        for i in range(n):
+            if not ind[i]: 
+                q.append(i)
+        
+        reach = [[False] * n for _ in range(n)]
+        
+        while q:
+            node = q.popleft()
+            for nei in g[node]:
+                reach[node][nei] = True
                 
-        return ans
+                for i in range(n):
+                    if reach[i][node]:
+                        reach[i][nei] = True
+                        
+                ind[nei] -= 1
+                if not ind[nei]:
+                    q.append(nei)
+                    
+                    
+        return [reach[u][v] for u, v in queries]
+                
             
         
         
