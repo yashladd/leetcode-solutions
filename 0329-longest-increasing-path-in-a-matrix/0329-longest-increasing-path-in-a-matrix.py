857ea79fa594310188f0e0bd2293e30fbdb73d0b
yashladd <43058610+yashladd@users.noreply.github.com>
2024-01-27 00:00:48 -0700

    Time: 298 ms (82.92%), Space: 21.4 MB (34.80%) - LeetHub

---
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        m, n = len(matrix), len(matrix[0])
+        neibs = [(0, -1), (0, 1), (1, 0), (-1, 0)]
+        
+        @lru_cache(None)
+        def dfs(x, y):
+            ans = 1
+            for dx, dy in neibs:
+                if 0 <= x+dx < m and 0 <= y+dy <n and matrix[x+dx][y+dy] < matrix[x][y]:
+                    ans = max(ans, dfs(x+dx, y+dy) + 1) 
+            return ans
+        
+        return max(dfs(i, j) for i, j in product(range(m), range(n)))
+            
+        
