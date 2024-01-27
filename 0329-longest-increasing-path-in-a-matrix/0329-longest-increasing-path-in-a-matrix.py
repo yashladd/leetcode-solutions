e04a2f231df3080b67f0e84dab0139061aba2804
yashladd <43058610+yashladd@users.noreply.github.com>
2024-01-27 00:42:27 -0700

    Time: 315 ms (74.76%), Space: 21.3 MB (40.70%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -1,16 +1,21 @@
 class Solution:
-    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
-        m, n = len(matrix), len(matrix[0])
-        neibs = [(0, -1), (0, 1), (1, 0), (-1, 0)]
+    def longestIncreasingPath(self, g: List[List[int]]) -> int:
+        n, m = len(g), len(g[0])
         
-        @lru_cache(None)
-        def dfs(x, y):
-            ans = 1
-            for dx, dy in neibs:
-                if 0 <= x+dx < m and 0 <= y+dy <n and matrix[x+dx][y+dy] < matrix[x][y]:
-                    ans = max(ans, dfs(x+dx, y+dy) + 1) 
-            return ans
+        def inb(i, j):
+            return i >= 0 and j >= 0 and i < n and j < m
         
-        return max(dfs(i, j) for i, j in product(range(m), range(n)))
-            
+        @cache
+        def dfs(i, j):
+            path = 1
+            for dx, dy in [(0,1), (1,0), (0,-1), (-1,0)]:
+                r, c = i + dx, j + dy
+                if inb(r,c) and g[r][c] > g[i][j]:
+                    path = max(path, 1 + dfs(r, c))
+                    
+            return path
+        
+        return max(dfs(i,j) for i, j in product(range(n), range(m)))
+        
+        
         
