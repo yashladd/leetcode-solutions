52cb2ebcecc7a0a54a6a0f12235ed5cf3767982a
yashladd <43058610+yashladd@users.noreply.github.com>
2024-01-26 19:02:05 -0700

    Time: 90 ms (76.29%), Space: 17 MB (81.28%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.py
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.py
@@ -7,17 +7,22 @@ class Solution:
         def inb(i, j):
             return i >= 0 and j >= 0 and i < n and j < n
         
-        vis = [[0] * n for _ in range(n)]
+        vis = [[float("inf")] * n for _ in range(n)]
+        vis[0][0] = grid[0][0]
         while h:
             time, (r, c) = heappop(h)
             if r == n-1 and c == n-1:
                 return time
-            if vis[r][c]:
-                continue
-            vis[r][c] = 1
+            # if vis[r][c]:
+            #     continue
+            # vis[r][c] = 1
             for dx, dy in [(0,1), (1, 0), (0,-1), (-1, 0)]:
                 x, y = r + dx, c + dy
-                if inb(x,y) and not vis[x][y]:
-                    heappush(h, (max(grid[x][y], time), (x, y)))
+                
+                if inb(x,y):
+                    newTime = max(grid[x][y], time)
+                    if newTime < vis[x][y]:
+                        vis[x][y] = newTime
+                        heappush(h, (max(grid[x][y], time), (x, y)))
                             
         
