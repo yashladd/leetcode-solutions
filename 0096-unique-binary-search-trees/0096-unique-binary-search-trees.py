6c89b45372ee18e4caae5c723b6cc8387b723ac1
yashladd <43058610+yashladd@users.noreply.github.com>
2024-01-31 16:52:47 -0700

    Time: 30 ms (90.55%), Space: 16.5 MB (71.28%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
@@ -11,10 +11,8 @@ class Solution:
             
             res = 0
             for i in range(x):
-                # print(i, range(0, i-1), range(i+1, x), len(range(0, i-1)), len(range(i+1, x)))
-                lef = f(len(range(0, i)))
-                rig = f(len(range(i+1, x)))
-                # print(res, lef, rig)
+                lef = f(i)
+                rig = f(x-i-1)
                 res += (lef * rig)
             return res
         
