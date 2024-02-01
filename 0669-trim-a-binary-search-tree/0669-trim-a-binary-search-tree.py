e561e6de9e7b91cc80cfe11294a18d02916ebcd3
yashladd <43058610+yashladd@users.noreply.github.com>
2024-02-01 13:33:45 -0700

    Time: 51 ms (40.37%), Space: 19.5 MB (77.55%) - LeetHub

--- 0669-trim-a-binary-search-tree/0669-trim-a-binary-search-tree.py
+++ 0669-trim-a-binary-search-tree/0669-trim-a-binary-search-tree.py
@@ -5,46 +5,17 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    
-    def reorder(self, root):
-        if not root.left:
-            return root.right
-        elif not root.right:
-            return root.left
-        
-        rCh = root.right
-        curr = root.left
-        while curr.right:
-            curr = curr.right
-        curr.right = rCh
-        return root.left
-    
-    def trimBST(self, root: Optional[TreeNode], low: int, high: int) -> Optional[TreeNode]:
-        
+    def trimBST(self, root: Optional[TreeNode], L: int, R: int) -> Optional[TreeNode]:
         if not root:
             return None
+        if root.val >= L and root.val <= R:
+            root.left = self.trimBST(root.left, L, R)
+            root.right = self.trimBST(root.right, L, R)
+            return root
         
-        if root.val < low or root.val > high:
-            root =  self.reorder(root)
-            if not root:
-                return root
-            else:
-                return self.trimBST(root, low, high)
-        
-        if root.left and (root.left.val < low or root.left.val > high):
-            root.left = self.reorder(root.left)
-            # if root.left:
-            #     root.left = self.trimBST(root.left, low, high)
-            
-        if root.right and (root.right.val < low or root.right.val > high):
-            root.right = self.reorder(root.right)
-            # if root.right:
-            #     root.right = self.trimBST(root.right, low, high)
-            
-        
-        root.left = self.trimBST(root.left, low, high)
-        root.right = self.trimBST(root.right, low, high)
-            
-        return root
-            
+        # All right children can be deleted
+        if root.val > R:
+            return self.trimBST(root.left, L, R)
+        # root.val < L: all left children can be deleted
+        return self.trimBST(root.right, L, R)
         
