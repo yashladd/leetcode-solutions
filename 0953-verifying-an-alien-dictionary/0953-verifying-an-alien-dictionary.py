42c1eb9ed6cd7bff27796beca3904f23ec8783fa
yashladd <43058610+yashladd@users.noreply.github.com>
2024-02-01 17:27:27 -0700

    Time: 38 ms (82.60%), Space: 16.7 MB (56.41%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
@@ -11,21 +11,13 @@ class Solution:
             if word == prev:
                 continue
             n1, n2 = len(prev), len(word)
-            i, j = 0, 0
-            matched = 0
-            while i < n1 and j < n2 and prev[i] == word[j]:
-                i += 1
-                j += 1
-            
-            if i == n1 or j == n2:
-                if n1 > n2 and j == n2:
+            for j in range(n1):
+                if j == n2:
                     return False
-                else:
-                    prev = word
-                    continue
-            if h[prev[i]] > h[word[j]]:
-                    return False
-            
+                if prev[j] != word[j]:
+                    if h[prev[j]] > h[word[j]]:
+                        return False
+                    break
             prev = word
             
         return True
