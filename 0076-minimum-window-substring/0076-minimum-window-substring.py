d1fa5078450e6c14e351842332a296088e57dfe5
yashladd <43058610+yashladd@users.noreply.github.com>
2024-02-06 13:06:58 -0700

    Time: 410 ms (13.70%), Space: 17.3 MB (52.99%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,28 +1,29 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        # s, t = s.lower(), t.lower()
-        tCnt = Counter(t)
-
-        i, j, n = 0, 0, len(s)
-        m = len(t)
-        matches = 0
-        sCnt = Counter()
-        mini = float("inf")
+        tF = Counter(t)
+        sF = Counter()
+        i, j = 0, 0
+        n = len(s)
         res = ""
+        mini = float("inf")
+        
+        
+        def check():
+            for k, cnt in tF.items():
+                if sF[k] < cnt:
+                    return False
+            return True
+        
         while j < n:
-            sCnt[s[j]] += 1
-            while sCnt >= tCnt:
-                if j - i + 1 < mini:
+            sF[s[j]] += 1
+            while check():
+                if j-i + 1 < mini:
                     mini = j - i + 1
                     res = s[i:j+1]
-                sCnt[s[i]] -= 1
+                sF[s[i]] -= 1
                 i += 1
             j += 1
-
+            
         return res
-
-
-
-
-
-        
+                
+                
