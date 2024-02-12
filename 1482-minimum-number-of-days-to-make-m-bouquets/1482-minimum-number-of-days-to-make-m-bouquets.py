bcb65a0431acb7602aa1cf781402e1efe5e84cc1
yashladd <43058610+yashladd@users.noreply.github.com>
2024-02-11 20:23:39 -0700

    Time: 1496 ms (11.32%), Space: 30.6 MB (58.52%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.py
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.py
@@ -1,34 +1,37 @@
 class Solution:
     def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
-        n = len(bloomDay)
-        def canMake(days):
-            boq = 0
-            flow = 0
-            for d in bloomDay:
-                if d <= days:
-                    flow += 1
-                else:
-                    boq += (flow//k)
-                    flow = 0
-                if boq == m:
-                    return True
-                    
-            boq += (flow//k)
-            return boq >= m
         
-        lo = min(bloomDay)
-        hi = max(bloomDay)
+        def canMake(d):
+            a = [1 if bloomDay[i] <= d else 0 for i in range(len(bloomDay))]
+            cnt = 0
+            i = 0 
+            while i < len(a):
+                if not a[i]:
+                    i += 1
+                    continue
+                else:
+                    fl = 1
+                    i += 1
+                    while i < len(a) and a[i] and fl < k:
+                        fl += 1
+                        i += 1
+                    if fl == k:
+                        cnt += 1
+            return cnt >= m
+        
+        l, r = 1, max(bloomDay)
+        
         ans = -1
-        while lo <= hi:
-            mid = (lo + hi) >> 1
-            if(canMake(mid)):
+        while l <= r:
+            mid = (l + r) >> 1
+            if canMake(mid):
                 ans = mid
-                hi = mid - 1
+                r = mid-1
             else:
-                lo = mid + 1
-                
+                l = mid + 1
         return ans
-                
             
-                    
+                        
+                        
+            
         
