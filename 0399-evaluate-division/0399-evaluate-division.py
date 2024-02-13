b11ba73d8f6a149fa20a9d1f92d0e07a4e6f331c
yashladd <43058610+yashladd@users.noreply.github.com>
2024-02-12 23:26:04 -0700

    Time: 42 ms (30.08%), Space: 16.6 MB (74.12%) - LeetHub

---
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -0,0 +1,37 @@
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        g = defaultdict(list)
+        vals = set()
+        for i, (u, v) in enumerate(equations):
+            g[u].append((v, values[i]))
+            g[v].append((u, 1/values[i]))
+            vals.add(u)
+            vals.add(v)
+
+        def bfs(sr, de):
+            found = False
+            q = deque([(sr, 1.0, -1)])
+            vis = set()
+            vis.add(sr)
+            while q:
+                node, cur, par = q.popleft()
+                if node == de:
+                    return cur
+                for nei, mul in g[node]:
+                    if nei != par and nei not in vis:
+                        vis.add(nei)
+                        q.append([nei, cur * mul, node])
+            if not found:
+                return -1.0
+        res= []
+        for u, v in queries:
+            if u not in vals or v not in vals:
+                res.append(-1)
+                continue
+            res.append(bfs(u, v))
+        return res 
+
+
+            
+
+        
