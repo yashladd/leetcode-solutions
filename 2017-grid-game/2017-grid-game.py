d9de97ba290296e90130ce2d92e7f06acf594190
yashladd <43058610+yashladd@users.noreply.github.com>
2024-02-19 12:05:28 -0700

    Time: 804 ms (85.40%), Space: 32.9 MB (70.59%) - LeetHub

--- 2017-grid-game/2017-grid-game.py
+++ 2017-grid-game/2017-grid-game.py
@@ -1,25 +1,13 @@
 class Solution:
     def gridGame(self, grid: List[List[int]]) -> int:
-        n = 2
-        m = len(grid[0])
-        choices = [[0] * m for _ in range(2) ]
-        post = 0
-        for col in range(m-1, 0, -1):
-            post = post + grid[0][col]
-            choices[0][col] = post
-        pre = 0
-        for col in range(0, m-1):
-            pre += grid[1][col]
-            choices[1][col] = pre
-        best = float("inf")
-        for i in range(m):
-            rightSum, leftSum = 0, 0
-            if i >= 1:
-                leftSum = choices[1][i-1]
-            if i <= m-2:
-                rightSum = choices[0][i+1]
-            best = min(best, max(rightSum, leftSum))
-        return best
+        n = len(grid[0])
+        ans = math.inf
+        topSum = sum(grid[0])
+        bottomSum = 0
 
+        for i in range(n):
+            topSum -= grid[0][i]
+            ans = min(ans, max(topSum, bottomSum))
+            bottomSum += grid[1][i]
 
-        
+        return ans
