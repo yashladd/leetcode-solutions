14e08278b8ba7dc6c7fe4af33d500c2c04364bb1
yashladd <43058610+yashladd@users.noreply.github.com>
2024-02-19 21:50:13 -0700

    Time: 45 ms (51.41%), Space: 16.4 MB (91.13%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -1,3 +1,7 @@
+class Cmp(str):
+    def __lt__(x,y):
+        return x + y > y + x
+
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
         
@@ -5,8 +9,8 @@ class Solution:
         def cmp(x, y):
             return -1 if x + y >= y + x else 1
         
-        a = sorted(map(str, nums), key=cmp_to_key(cmp))
-        return "0" if a[0] == "0" else "".join(a)
+        a = sorted(map(str, nums), key=Cmp)
+        return str(int("".join(a)))
         
         
         
