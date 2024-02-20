357a4554c3eac2524a953b24f9e7d783149299e2
yashladd <43058610+yashladd@users.noreply.github.com>
2024-02-19 17:54:39 -0700

    Time: 31 ms (82.93%), Space: 16.6 MB (69.96%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,31 +1,11 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         if needle == "": return 0
+        if needle == haystack: return 0
         n, m = len(haystack), len(needle)
-        lps = [0] * m
-        prevLps, i = 0, 1
-        # Precomputing LPS
-        while i < m:
-            if needle[i] == needle[prevLps]:
-                lps[i] = prevLps + 1
-                i += 1
-                prevLps += 1
-            elif prevLps == 0:
-                lps[i] = 0                
-                i += 1
-            else:
-                # We stay at the current index, perhaps some prefix matches this suffix
-                prevLps = lps[prevLps-1]
+        
+        for i in range(n-m+1):
+            if haystack[i:i+m] == needle:
+                return i
 
-        i, j = 0, 0
-        while i < n:
-            if haystack[i] == needle[j]:
-                i, j = i + 1, j + 1
-            else:
-                if j == 0:
-                    i += 1
-                else:
-                    j = lps[j-1]
-            if j == m:
-                return i - m
         return -1
