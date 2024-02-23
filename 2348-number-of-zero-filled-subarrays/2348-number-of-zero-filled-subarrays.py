9bffb96019e4228d52ff5bb113cf15b37accaf58
yashladd <43058610+yashladd@users.noreply.github.com>
2024-02-22 22:11:53 -0700

    Time: 787 ms (87.77%), Space: 27 MB (88.22%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.py
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.py
@@ -1,14 +1,12 @@
 class Solution:
-    def zeroFilledSubarray(self, a: List[int]) -> int:
-        i = 0
-        res = 0
-        n = len(a)
-        while i < n:
-            if a[i] == 0:
-                l = i
-                while i < n and a[i] == 0:
-                    i += 1
-                z = i - l
-                res += z * (z + 1) // 2
-            i += 1
-        return res
+    def zeroFilledSubarray(self, nums: List[int]) -> int:
+        total_zero_subarrays = current_zero_subarrays = 0
+                
+        for num in nums:
+            if num == 0:
+                current_zero_subarrays += 1
+                total_zero_subarrays += current_zero_subarrays
+            else:
+                current_zero_subarrays = 0
+                
+        return total_zero_subarrays
