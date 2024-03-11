4ca62b506527e8d6bc513240b7015d81d6f6b32b
yashladd <43058610+yashladd@users.noreply.github.com>
2024-03-10 20:12:37 -0600

    Time: 45 ms (5.71%), Space: 16.5 MB (61.09%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.py
+++ 0791-custom-sort-string/0791-custom-sort-string.py
@@ -1,11 +1,22 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
         h = defaultdict(int)
-        cnt = 1
+        cnt = 0
         for x in order:
             h[x] = cnt
             cnt += 1
+        orders = [[] for _ in range(26)]
+        for ch in s:
+            if ch in h:
+                orders[h[ch]].append(ch)
+            else:
+                orders[-1].append(ch)
+                
+        res = []
+        for o in orders:
+            if o:
+                res.extend(o)
         
-        return "".join(sorted(s, key = lambda x: h[x]))
+        return "".join(res)
 
         
