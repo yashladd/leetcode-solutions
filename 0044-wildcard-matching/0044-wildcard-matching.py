cf3536394cf1445ec4cff8ca6ea5d63591aec460
yashladd <43058610+yashladd@users.noreply.github.com>
2025-11-11 14:36:32 -0800

    Time: 394 ms (57.24%), Space: 145.6 MB (5.01%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.py
+++ 0044-wildcard-matching/0044-wildcard-matching.py
@@ -1,20 +1,26 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        n, m = len(s), len(p)
-
-        @lru_cache(None)
+        """
+        a   a
+        a * a
+        """
+        @cache
         def f(i, j):
             if i < 0 or j < 0:
-                if j >= 0 and p[j] == "*":
-                    return f(i, j-1)
-                return i == j
+                if i < 0:
 
-            if s[i] == p[j] or p[j] == "?":
-                return f(i-1, j-1)
+                    if j < 0:
+                        return True
+                    if p[j] == "*":
+                        return f(i, j-1)
+                return False
+
+            if s[i] == p[j] or p[j] == "?": return f(i-1, j-1)
 
             if p[j] == "*":
                 return f(i-1, j) or f(i, j-1)
-
             return False
 
+        n, m = len(s), len(p)
         return f(n-1, m-1)
+        
