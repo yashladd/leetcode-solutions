fe3eea9ef4a0e19ebdfc8180cff665a147677db5
yashladd <43058610+yashladd@users.noreply.github.com>
2025-11-23 09:58:31 -0800

    Time: 334 ms (5.91%), Space: 28.3 MB (13.02%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.py
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.py
@@ -1,9 +1,32 @@
 class Solution:
     def maxSumDivThree(self, nums: List[int]) -> int:
-        f = [0, -float("inf"), -float("inf")]
-        for num in nums:
-            g = f[:]
-            for i in range(3):
-                g[(i + num % 3) % 3] = max(g[(i + num % 3) % 3], f[i] + num)
-            f = g
-        return f[0]
+        
+        n = len(nums)
+
+        dp = [[-float("inf") for _ in range(3)] for _ in range(n+1)]
+
+        for i in range(n+1):
+            dp[i][0] = 0
+
+        for i in range(1, n+1):
+            for m in range(3):
+                best = -float('inf')
+                best = max(best, dp[i-1][(nums[i-1] + m)%3] + nums[i-1])
+                best = max(best, dp[i-1][m])
+                dp[i][m] = best
+
+        return dp[n][0]
+
+
+        @cache
+        def f(i, m):
+            if i < 0:
+                return 0 if not m else -float("inf")
+
+            best = f(i-1, (nums[i] + m)%3) + nums[i]
+
+            best = max(best, f(i-1, m))
+
+            return best
+
+        return f(n-1, 0)
