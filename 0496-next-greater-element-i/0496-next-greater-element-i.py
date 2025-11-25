4141551dc99780db3c49c2dbcb081bc12bb82654
yashladd <43058610+yashladd@users.noreply.github.com>
2025-11-25 14:38:10 -0800

    Time: 0 ms (100%), Space: 18.2 MB (20.56%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,17 +1,21 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        h = {n:i for i, n in enumerate(nums1)}
-        
+        h = { x:i for i, x in enumerate(nums1) }
+        res = [-1 for x in nums1]
         stk = []
-        res = [-1] * len(nums1)
-        
-        for n in nums2:
-            while stk and stk[-1] < n:
-                num = stk.pop()
-                pos = h[num]
-                res[pos] = n
-            if n in h:
-                stk.append(n)
-            
-        return res
+
+        for i in range(len(nums2)-1, -1, -1):
+            while stk and stk[-1] <= nums2[i]:
+                stk.pop()
+            if stk and nums2[i] in h:
+                res[h[nums2[i]]] = stk[-1]
+            stk.append(nums2[i])
+
+        # for i, x in enumerate(nums2):
+        #     while stk and stk[-1] < x:
+        #         y = stk.pop()
+        #         if y in h:
+        #             res[h[y]] = x
+        #     stk.append(x)
+        return res 
         
