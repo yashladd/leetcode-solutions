be99828312770d542f4d5fd79e84944a017a0a8a
yashladd <43058610+yashladd@users.noreply.github.com>
2025-11-26 16:05:27 -0800

    Time: 47 ms (43.02%), Space: 32.6 MB (58.95%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
@@ -1,24 +1,71 @@
+class DS:
+    def __init__(self, n):
+        self.p = [i for i in range(n+1)]
+        self.s = [1] * (n+1)
+
+    def find(self, x):
+        if x == self.p[x]:
+            return x
+        self.p[x] = self.find(self.p[x])
+        return self.p[x]
+
+    def union(self, u, v):
+        pU, pV = self.find(u), self.find(v)
+        if pU == pV:
+            return False
+        if self.s[pU] < self.s[pV]:
+            pU, pV = pV, pU
+
+        self.p[pU] = pV
+        self.s[pU] += self.s[pV]
+        return True
+        
+
+
 class Solution:
     def makeConnected(self, n: int, connections: List[List[int]]) -> int:
-        if len(connections) < n-1: return -1
-        
-        G = defaultdict(set)
+        g = defaultdict(list)
+
+        comp = 0
+
+        vis = set()
+        ds = DS(n)
+
+        extra = 0
         for u, v in connections:
-            G[u].add(v)
-            G[v].add(u)
-            
-        vis = {}
-        def dfs(node):
-            vis[node] = 1
-            for nei in G[node]:
+            if not ds.union(u, v):
+                extra += 1
+
+        comps = 0
+        for i in range(n):
+            if i == ds.find(i):
+                comps += 1
+        print(extra, comps)
+        if extra < comps - 1:
+            return -1
+
+        return comps - 1
+
+        def dfs(x):
+            vis.add(x)
+
+            for nei in g[x]:
                 if nei not in vis:
                     dfs(nei)
-                    
-        comp = 0
+        
         for i in range(n):
+            print(i, vis)
             if i not in vis:
                 dfs(i)
                 comp += 1
-                
+
+
+        """
+         3
+         |
+         0 - 1
+         | 
+         2
+        """
+
         return comp - 1
-        
