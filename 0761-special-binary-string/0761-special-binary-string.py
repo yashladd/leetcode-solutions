87ceb6dd2276aa660228b46b83a2482c1fab62a2
yashladd <43058610+yashladd@users.noreply.github.com>
2025-12-05 12:26:57 -0800

    Time: 0 ms (100%), Space: 17.5 MB (94.91%) - LeetHub

--- 0761-special-binary-string/0761-special-binary-string.py
+++ 0761-special-binary-string/0761-special-binary-string.py
@@ -6,7 +6,7 @@ class Solution:
         for r, c in enumerate(s):
             cnt += 1 if c == "1" else -1
             if cnt == 0:
-                inner = self.makeLargestSpecial(s[l+1:r+1])
+                inner = self.makeLargestSpecial(s[l+1:r])
                 res.append("1" + inner + "0")
                 cnt = 0
                 l = r + 1
