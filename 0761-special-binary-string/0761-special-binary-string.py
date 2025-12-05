0faadd3997e73c599be618637e63cab1ef71991d
yashladd <43058610+yashladd@users.noreply.github.com>
2025-12-05 12:26:20 -0800

    Time: 1 ms (23.77%), Space: 17.6 MB (94.91%) - LeetHub

--- 0761-special-binary-string/0761-special-binary-string.py
+++ 0761-special-binary-string/0761-special-binary-string.py
@@ -1,26 +1,16 @@
 class Solution:
     def makeLargestSpecial(self, s: str) -> str:
-        count = 0
-        i = 0
+        l = 0
         res = []
+        cnt = 0
+        for r, c in enumerate(s):
+            cnt += 1 if c == "1" else -1
+            if cnt == 0:
+                inner = self.makeLargestSpecial(s[l+1:r+1])
+                res.append("1" + inner + "0")
+                cnt = 0
+                l = r + 1
+
+        return "".join(sorted(res, reverse=True))
+
         
-        # Iterate through the string to find top-level special substrings
-        for j, char in enumerate(s):
-            count += 1 if char == '1' else -1
-            
-            # When count is 0, we found a balanced substring s[i:j+1]
-            if count == 0:
-                # 1. Identify the inner part: s[i+1:j] (strip outer 1 and 0)
-                # 2. Recurse on the inner part to maximize it
-                # 3. Add the outer 1 and 0 back
-                inner_maximized = self.makeLargestSpecial(s[i+1:j])
-                res.append('1' + inner_maximized + '0')
-                
-                # Move start pointer to the next character
-                i = j + 1
-        
-        # Sort the processed substrings in descending order to get lexicographical max
-        # Example: ['10', '1100'] becomes ['1100', '10']
-        res.sort(reverse=True)
-        
-        return "".join(res)
