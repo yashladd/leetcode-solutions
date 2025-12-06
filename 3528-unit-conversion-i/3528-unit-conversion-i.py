343b7b03a63d2108d05275f92f8486790e369969
yashladd <43058610+yashladd@users.noreply.github.com>
2025-12-05 16:21:23 -0800

    Time: 323 ms (59.43%), Space: 84.8 MB (57.79%) - LeetHub

--- 3528-unit-conversion-i/3528-unit-conversion-i.py
+++ 3528-unit-conversion-i/3528-unit-conversion-i.py
@@ -9,21 +9,16 @@ class Solution:
             g[u].append((v, w))
 
         q = deque([(0, 1)])
-        vis = set()
-        vis.add(0)
         while q:
             idx, wt = q.popleft()
             for nextIdx, w in g[idx]:
-                if nextIdx not in vis:
+                if nextIdx not in h:
                     currWt = (wt * w) % MOD
-                    vis.add((nextIdx, currWt))
                     h[nextIdx] = currWt
                     q.append((nextIdx, currWt))
 
         res = [1] * (n+1)
-        print(h, res)
         for idx, wt in h.items():
-            print(idx, wt)
             res[idx] = wt
 
         return res
