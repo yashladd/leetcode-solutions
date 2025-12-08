f1d1dc9539cd1e4f77b59c0a294648570932bd5d
yashladd <43058610+yashladd@users.noreply.github.com>
2025-12-08 13:36:45 -0800

    Time: 239 ms (47.36%), Space: 0B (100%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,6 +1,6 @@
 -- Write your PostgreSQL query statement below
 
 select e.name as Employee
-from Employee e
-left join Employee m on e.managerId = m.id
-where e.salary > m.salary;
+from Employee e, Employee m
+where e.managerId = m.id
+and e.salary > m.salary;
