93dea8da2454febc2141c98746d00e92f1cab382
yashladd <43058610+yashladd@users.noreply.github.com>
2025-12-15 15:14:36 -0800

    Time: 116 ms (12.9%), Space: 48 MB (12%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,48 +1,56 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
-    boolean hasMoreChars(Map<Character, Integer> sF, Map<Character, Integer> tF) {
-        for (Map.Entry<Character, Integer> e: tF.entrySet()) {
-            Character c = e.getKey();
-            Integer cnt = e.getValue();
-            if (sF.getOrDefault(c, 0) < cnt) return true;
-        }
-        return false;
-    }
     public String minWindow(String s, String t) {
         Map<Character, Integer> sF = new HashMap<>();
         Map<Character, Integer> tF = new HashMap<>();
-        int mini = Integer.MAX_VALUE, l = 0;
+
         int n1 = s.length(), n2 = t.length();
+        if (n2 > n1) return "";
+
+        for (char c : t.toCharArray()) {
+            tF.put(c, tF.getOrDefault(c, 0) + 1);
+        }
+
+        int required = tF.size(); // distinct chars in t that must be satisfied
+        int formed = 0;           // how many are currently satisfied
+
+        int mini = Integer.MAX_VALUE;
+        int bestL = 0;
+        int l = 0;
         String res = "";
-
-        if (n2 > n1) return res;
-
-        for (char c: t.toCharArray()) {
-            tF.put( c, tF.getOrDefault((c), 0) + 1);
-        } 
-
-        System.out.println(tF);
-
         for (int r = 0; r < n1; r++) {
             char c = s.charAt(r);
-            sF.put(c, sF.getOrDefault(c, 0) + 1);
+            int newCount = sF.getOrDefault(c, 0) + 1;
+            sF.put(c, newCount);
 
-            while (!hasMoreChars(sF, tF)) {
-            // 1. Update the result (since the window is currently valid)
+            // If this char is required and we just met its required count, formed++
+            if (tF.containsKey(c) && newCount == tF.get(c)) {
+                formed++;
+            }
+
+            // shrink only while window is valid
+            while (formed == required) {
                 if (r - l + 1 < mini) {
                     mini = r - l + 1;
+                    bestL = l;
                     res = s.substring(l, r + 1);
                 }
-                
+
                 char leftChar = s.charAt(l);
-                sF.put(leftChar, sF.getOrDefault(leftChar, 0) - 1); 
-                
+                int leftCount = sF.get(leftChar) - 1;
+                sF.put(leftChar, leftCount);
+
+                // If leftChar is required and we just dropped below required, formed--
+                if (tF.containsKey(leftChar) && leftCount < tF.get(leftChar)) {
+                    formed--;
+                }
+
                 l++;
             }
         }
 
-        
-
         return res;
-
     }
 }
