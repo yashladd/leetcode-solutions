1701f80200270a9b67e9747048f65c4ae531627a
yashladd <43058610+yashladd@users.noreply.github.com>
2025-12-20 11:43:11 -0800

    Time: 7 ms (56.38%), Space: 19.8 MB (45.74%) - LeetHub

--- 0529-minesweeper/0529-minesweeper.py
+++ 0529-minesweeper/0529-minesweeper.py
@@ -25,26 +25,33 @@ class Solution:
             board[r][c] = "X"
             return board
 
-        queue = deque([(r, c)])
-
         def inb(i, j):
             return i >= 0 and i < N and j >= 0 and j < M
         vis = set()
-        vis.add((r, c))
-        while queue:
-            x, y = queue.popleft()
+
+        def dfs(x, y):
+            vis.add((x, y))
             cnt = 0
             for dx, dy in [(-1, 0), (0, -1), (1, 0), (0, 1), (1, 1), (-1, -1), (1, -1), (-1, 1)]:
                 p, q = x + dx, y + dy
                 if inb(p, q) and board[p][q] == "M":
                     cnt += 1
             board[x][y] = str(cnt) if cnt else "B"
-            print(x, y, cnt)
             if cnt == 0:
                 for dx, dy in [(-1, 0), (0, -1), (1, 0), (0, 1), (1, 1), (-1, -1), (1, -1), (-1, 1)]:
                     p, q = x + dx, y + dy
                     if (inb(p, q)) and (p, q) not in vis:
-                        queue.append((p, q))
-                        vis.add((p, q))
+                        dfs(p, q)
 
+        # while queue:
+        #     x, y = queue.popleft()
+        #     c
+
+        #     if cnt == 0:
+        #         for dx, dy in [(-1, 0), (0, -1), (1, 0), (0, 1), (1, 1), (-1, -1), (1, -1), (-1, 1)]:
+        #             p, q = x + dx, y + dy
+        #             if (inb(p, q)) and (p, q) not in vis:
+        #                 queue.append((p, q))
+        #                 vis.add((p, q))
+        dfs(r, c)
         return board
