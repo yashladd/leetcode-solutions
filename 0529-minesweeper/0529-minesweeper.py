704ae51adc07009c362f6e7f26fdabb64f1c4c9d
yashladd <43058610+yashladd@users.noreply.github.com>
2025-12-20 11:38:10 -0800

    Time: 11 ms (21.59%), Space: 18.3 MB (95.12%) - LeetHub

---
+++ 0529-minesweeper/0529-minesweeper.py
@@ -0,0 +1,50 @@
+class Solution:
+    def updateBoard(self, board: List[List[str]], click: List[int]) -> List[List[str]]:
+        """
+
+[
+
+["E","E","E","E","E"],
+["E","E","M","E","E"],
+["E","E","E","E","E"],
+["E","E","E","E","E"]], click = [3,0]
+
+
+["B","1","E","1","B"],
+["B","1","M","1","B"],
+["B","1","1","1","B"],
+["B","B","B","B","B"]
+
+]
+        """
+
+        r, c = click
+        N, M = len(board), len(board[0])
+
+        if board[r][c] == "M":
+            board[r][c] = "X"
+            return board
+
+        queue = deque([(r, c)])
+
+        def inb(i, j):
+            return i >= 0 and i < N and j >= 0 and j < M
+        vis = set()
+        vis.add((r, c))
+        while queue:
+            x, y = queue.popleft()
+            cnt = 0
+            for dx, dy in [(-1, 0), (0, -1), (1, 0), (0, 1), (1, 1), (-1, -1), (1, -1), (-1, 1)]:
+                p, q = x + dx, y + dy
+                if inb(p, q) and board[p][q] == "M":
+                    cnt += 1
+            board[x][y] = str(cnt) if cnt else "B"
+            print(x, y, cnt)
+            if cnt == 0:
+                for dx, dy in [(-1, 0), (0, -1), (1, 0), (0, 1), (1, 1), (-1, -1), (1, -1), (-1, 1)]:
+                    p, q = x + dx, y + dy
+                    if (inb(p, q)) and (p, q) not in vis:
+                        queue.append((p, q))
+                        vis.add((p, q))
+
+        return board
