4481fa85d769e2aa1ef4f1c14c8e5943425d5901
yashladd <43058610+yashladd@users.noreply.github.com>
2025-12-21 11:50:31 -0800

    Time: 0 ms (100%), Space: 17.4 MB (98.19%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.py
+++ 0140-word-break-ii/0140-word-break-ii.py
@@ -1,44 +1,23 @@
+
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
-        # 1. Quick lookup (O(1))
-        wordSet = set(wordDict)
+
+        res = []
+
+        N = len(s)
+
+        words = set(wordDict)
+
+        def rec(i, currWords):
+            if i == N:
+                if len(currWords):
+                    res.append(" ".join(currWords))
+
+            for j in range(i+1, N+1):
+                currWord = s[i:j]
+                if currWord in words:
+                    rec(j, currWords + [currWord])
         
-        # 2. Memoization Cache
-        memo = {}
+        rec(0, [])
 
-        def rec(i):
-            # If we already computed this index, return the saved list of sentences
-            if i in memo:
-                return memo[i]
-            
-            # Base Case: Reached end of string. 
-            # Return a list with empty string to indicate a valid path was found.
-            if i == len(s):
-                return [""]
-            
-            results = []
-            
-            # Loop to find valid words starting at 'i'
-            for j in range(i, len(s)):
-                current_word = s[i : j+1]
-                
-                # *** FIX: Replace 'is_valid_word' with actual check ***
-                if current_word in wordSet:
-                    
-                    # RECURSIVE STEP: Get all valid suffixes (sentence endings)
-                    suffixes = rec(j + 1)
-                    
-                    # COMBINE: Join current_word with every valid suffix
-                    for suffix in suffixes:
-                        if suffix == "":
-                            # We reached the end, so just add the word itself
-                            results.append(current_word)
-                        else:
-                            # We have more words, so add a space
-                            results.append(current_word + " " + suffix)
-            
-            # Cache the result for index 'i'
-            memo[i] = results
-            return results
-
-        return rec(0)
+        return res        
