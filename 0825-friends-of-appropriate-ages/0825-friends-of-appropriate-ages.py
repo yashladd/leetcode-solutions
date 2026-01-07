52a80e219e5d9f64d370ebf788fe1bc258e042a4
yashladd <43058610+yashladd@users.noreply.github.com>
2026-01-07 06:17:29 -0800

    Time: 107 ms (26.64%), Space: 20.3 MB (9.72%) - LeetHub

--- 0825-friends-of-appropriate-ages/0825-friends-of-appropriate-ages.py
+++ 0825-friends-of-appropriate-ages/0825-friends-of-appropriate-ages.py
@@ -3,14 +3,22 @@ class Solution(object):
         count = [0] * 121
         for age in ages:
             count[age] += 1
-
+            
         ans = 0
-        for ageA, countA in enumerate(count):
-            for ageB, countB in enumerate(count):
-                if ageA * 0.5 + 7 >= ageB: continue
-                if ageA < ageB: continue
-                if ageA < 100 < ageB: continue
-                ans += countA * countB
-                if ageA == ageB: ans -= countA
-
+        # iterate 1 to 120. x and y represent the ages.
+        for x, count_x in enumerate(count):
+            for y, count_y in enumerate(count):
+                
+                # The conditions from the problem description
+                if y <= 0.5 * x + 7: continue
+                if y > x: continue
+                if y > 100 and x < 100: continue
+                
+                # Add total possible connections between these two age groups
+                ans += count_x * count_y
+                
+                # If ages are the same, remove self-requests
+                if x == y:
+                    ans -= count_x
+                    
         return ans
