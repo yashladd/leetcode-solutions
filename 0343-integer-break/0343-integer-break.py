daac71d19c9f62a60f6bb438a5f51fff4a226b48
yashladd <43058610+yashladd@users.noreply.github.com>
2026-01-10 11:42:39 -0800

    Time: 3 ms (49.93%), Space: 19.6 MB (6.73%) - LeetHub

--- 0343-integer-break/0343-integer-break.py
+++ 0343-integer-break/0343-integer-break.py
@@ -8,27 +8,16 @@ class Solution:
 
                f(7) -> 1 * f(6), 2 * f(5), 3 * f(4)     
         """
+        if n <= 3:
+            return n-1
+        @cache
+        def f(x):
+            if x <= 3:
+                return x
+            maxi = 0
+            for i in range(2, x):
+                maxi = max(maxi, i * f(x-i))
 
-        dp = [ 0 ] * (n+1)
-        dp[1] = 1
-
-        for i in range(2, n+1):
-            maxi = 0 if i == n else i
-            for x in range(1, i):
-                maxi = max(maxi, dp[x] * dp[i-x])
-            dp[i] = maxi
-        return dp[n]
-
-
-        def f(i):
-            if i == 1:
-                return 1
-            
-            maxi = 0 if i == n else i
-            for x in range(1, i):
-                maxi = max(maxi, f(x) * f(i-x))
             return maxi
 
         return f(n)
-
-        
