7dba9eee714e9ac9a8eb1214e085d8fa7c6c1b07
yashladd <43058610+yashladd@users.noreply.github.com>
2026-01-10 08:21:54 -0800

    Time: 5 ms (2.22%), Space: 47.9 MB (5.38%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.java
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.java
@@ -1,25 +1,49 @@
 class Solution {
     public int countBattleships(char[][] board) {
-        int m = board.length;
-        int n = board[0].length;
-        int count = 0;
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                // If current cell is empty, move on
-                if (board[i][j] == '.') continue;
+    int n = board.length;
+    int m = board[0].length;
+    boolean[][] vis = new boolean[n][m];
+    int cnt = 0;
+    
+    // Direction array for moving Up, Down, Left, Right
+    int[][] DIRS = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            // Found a new, unvisited ship part
+            if (board[i][j] == 'X' && !vis[i][j]) {
+                cnt++; // Increment count for this new ship
                 
-                // Check if this is a part of an existing battleship detected from above
-                if (i > 0 && board[i - 1][j] == 'X') continue;
+                // --- Start BFS to mark the rest of this ship ---
+                Queue<int[]> q = new LinkedList<>();
+                q.offer(new int[]{i, j});
+                vis[i][j] = true;
                 
-                // Check if this is a part of an existing battleship detected from the left
-                if (j > 0 && board[i][j - 1] == 'X') continue;
-                
-                // If we passed the checks, we found the 'head' of a new battleship
-                count++;
+                while (!q.isEmpty()) {
+                    int[] curr = q.poll();
+                    int r = curr[0];
+                    int c = curr[1];
+                    
+                    for (int[] dir : DIRS) {
+                        int nr = r + dir[0];
+                        int nc = c + dir[1];
+                        
+                        // Check bounds using your helper, ensuring it's an 'X', and not visited
+                        if (inb(nr, nc, n, m) && board[nr][nc] == 'X' && !vis[nr][nc]) {
+                            vis[nr][nc] = true; // Mark as part of current ship
+                            q.offer(new int[]{nr, nc});
+                        }
+                    }
+                }
+                // --- End BFS ---
             }
         }
-        
-        return count;
     }
+    return cnt;
+}
+
+// Your existing helper function
+boolean inb(int nr, int nc, int n, int m) {
+    return nr >= 0 && nc >= 0 && nr < n && nc < m;
+}
 }
