3a1aadb0df6c296535b31d73dd229ec259f13707
yashladd <43058610+yashladd@users.noreply.github.com>
2026-01-09 17:34:29 -0800

    Time: 11 ms (93.48%), Space: 19.4 MB (12.32%) - LeetHub

--- 1638-count-substrings-that-differ-by-one-character/1638-count-substrings-that-differ-by-one-character.py
+++ 1638-count-substrings-that-differ-by-one-character/1638-count-substrings-that-differ-by-one-character.py
@@ -3,9 +3,17 @@ class Solution:
         res = 0
         for i in range(len(s)):
             for j in range(len(t)):
-                miss, pos = 0, 0
-                while i + pos < len(s) and j + pos < len(t) and miss < 2:
-                    miss += s[i + pos] != t[j + pos]
-                    res += miss == 1
-                    pos += 1
+                if s[i] != t[j]:
+                    l, r = 1, 1
+                    while i - l >= 0 and j - l >= 0 and s[i-l] == t[j-l]:
+                        l+=1
+                    while i + r < len(s) and j + r < len(t) and s[i+r] == t[j + r]:
+                        r += 1
+                    res += (l * r )
+
+
+                # while i + pos < len(s) and j + pos < len(t) and miss < 2:
+                #     miss += s[i + pos] != t[j + pos]
+                #     res += miss == 1
+                #     pos += 1
         return res
