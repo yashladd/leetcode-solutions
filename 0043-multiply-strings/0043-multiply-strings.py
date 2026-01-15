83827828876f530a3f8a450d351a6313201983ae
yashladd <43058610+yashladd@users.noreply.github.com>
2026-01-15 09:21:52 -0800

    Time: 47 ms (39.63%), Space: 19.5 MB (5.12%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -1,5 +1,23 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        res = int(num1) * int(num2)
-        return str(res)
+        """
+
+        1 3 5
+        x 9 9
+          8 1
+
+        """
+        n1 , n2 = len(num1) , len(num2)
+        if "0" in [num1, num2]: return "0"
+        
+        one, two = num1[::-1], num2[::-1]
+        res = [0] * (n1 + n2)
+        for i, c1 in enumerate(one):
+            for j, c2 in enumerate(two):
+                mul = int(c1) * int(c2)
+                res[i + j] += mul
+                res[i + j + 1] += (res[i + j] //10)
+                res[i+j] %= 10
+        return "".join(list(map(str, res[::-1]))).lstrip("0")
+
         
