cccdeb247663b91350a626cb04ac996b9b937d24
yashladd <43058610+yashladd@users.noreply.github.com>
2026-01-16 11:02:42 -0800

    Time: 15 ms (50.38%), Space: 47.7 MB (55.31%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,48 +1,28 @@
-
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        int n = nums.length;
 
-        // 1. Count Frequencies (Equivalent to Python's 'h = defaultdict(int)' and the first loop)
-        // Key: Number, Value: Frequency
-        Map<Integer, Integer> counts = new HashMap<>();
-        for (int num : nums) {
-            counts.put(num, counts.getOrDefault(num, 0) + 1);
+        Map<Integer, Integer> mp = new HashMap<>();
+
+        for (int n:nums) {
+            mp.put(n, mp.getOrDefault(n, 0) + 1);
         }
 
-        // 2. Create Frequency Buckets (Equivalent to Python's 'freq = [[] for _ in range(n + 1)]')
-        // The array index is the frequency (0 to n). The value is a List of numbers 
-        // that share that frequency.
-        // We use a raw array of List<Integer> and must initialize each index.
-        List<Integer>[] freqBuckets = new List[n + 1];
-        for (int i = 0; i < freqBuckets.length; i++) {
-            freqBuckets[i] = new ArrayList<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> Integer.compare(mp.get(a), mp.get(b)));
+
+        for (Map.Entry<Integer, Integer> e: mp.entrySet()) {
+            pq.add(e.getKey());
+            if (pq.size() > k) pq.poll();
         }
 
-        // Populate the buckets (Equivalent to Python's 'for num, f in h.items(): freq[f].append(num)')
-        for (int num : counts.keySet()) {
-            int freq = counts.get(num);
-            freqBuckets[freq].add(num);
+        int[] res = new int[k];
+
+        int idx = 0;
+
+        while (!pq.isEmpty()) {
+            res[idx++] = pq.poll();
         }
 
-        // 3. Collect Top K Results (Equivalent to Python's final loop)
-        int[] result = new int[k];
-        int resultIndex = 0;
+        return res;
         
-        // Iterate backward from the highest possible frequency (n)
-        for (int i = n; i >= 0 && resultIndex < k; i--) {
-            // Check if the current bucket (frequency level) has any numbers
-            for (int num : freqBuckets[i]) {
-                result[resultIndex++] = num;
-            }
-        }
-        
-        return result;
     }
 }
