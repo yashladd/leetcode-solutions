9fe6ce935ae42a6c410edd8de3155d903e3f9ce3
yashladd <43058610+yashladd@users.noreply.github.com>
2026-01-17 13:20:09 -0800

    Time: 118 ms (43.31%), Space: 19.8 MB (12.16%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -7,4 +7,4 @@ class Solution:
         for bet, cnt in sC.items():
             # print(bet, cnt , tC[bet])
             res += max(0, cnt - tC[bet])
-        return sum( list(x if x > 0 else 0 for x in (sC - tC).values()))
+        return res
