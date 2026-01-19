ee198d3228831b50fd1b8e402d8b3cb9426e1c11
yashladd <43058610+yashladd@users.noreply.github.com>
2026-01-19 13:24:34 -0800

    Time: 19 ms (64.16%), Space: 19.5 MB (13.77%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,26 +1,34 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        tf = defaultdict(int)
-        for ch in s1:
-            tf[ch] += 1
-        sf = defaultdict(int)
-        l  = 0
-        def check(tf, sf):
-            for k, v in tf.items():
-                if k not in sf or sf[k] != v:
-                    return False
-            return True
-        for r in range(len(s2)):
-            sf[s2[r]] += 1
-            print(sf)
-            if r >= len(s1):
-                sf[s2[l]] -= 1
-                if not sf[s2[l]]:
-                    del sf[s2[l]]
-                l += 1
-            if check(tf, sf):
+        if len(s1) > len(s2):
+            return False
+
+        s1Count, s2Count = [0] * 26, [0] * 26
+        for i in range(len(s1)):
+            s1Count[ord(s1[i]) - ord('a')] += 1
+            s2Count[ord(s2[i]) - ord('a')] += 1
+
+        matches = 0
+        for i in range(26):
+            matches += (1 if s1Count[i] == s2Count[i] else 0)
+
+        l = 0
+        for r in range(len(s1), len(s2)):
+            if matches == 26:
                 return True
 
-        return False
+            index = ord(s2[r]) - ord('a')
+            s2Count[index] += 1
+            if s1Count[index] == s2Count[index]:
+                matches += 1
+            elif s1Count[index] + 1 == s2Count[index]:
+                matches -= 1
 
-        
+            index = ord(s2[l]) - ord('a')
+            s2Count[index] -= 1
+            if s1Count[index] == s2Count[index]:
+                matches += 1
+            elif s1Count[index] - 1 == s2Count[index]:
+                matches -= 1
+            l += 1
+        return matches == 26
