8c1e00a5b4112a417c7e276eb2a1740324358f7e
yashladd <43058610+yashladd@users.noreply.github.com>
2026-01-20 13:12:47 -0800

    Time: 0 ms (100%), Space: 19.4 MB (16.47%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.py
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -1,34 +1,14 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-        """
-        [[10,20],[30,200],[400,50],[30,20]]
-        aCosts = [10, 30, 30, 400]
-        bCosts = [20, 20, 50, 200]
-
-        """
-        n = len(costs) // 2
-        N=len(costs)
-        # print(f"N {n}")
-        @cache
-        def f(i, a, b):
-            # print(f"f {i} {a} {b}")
-            if i == N:
-                if a == n and b == n:
-                    return 0
-                return float("inf")
-
-            takeA = float("inf")
-            if a + 1 <= n:
-                takeA = costs[i][0] + f(i+1, a + 1, b)
-
-            takeB = float("inf")
-            if b + 1 <= n:
-                takeB = costs[i][1] + f(i+1, a, b + 1)
-
-            return min(takeA, takeB)
-        val = f(0, 0, 0)
-        # print(val)
-        return val
-            
-
+        # Sort by a gain which company has 
+        # by sending a person to city A and not to city B
+        costs.sort(key = lambda x : x[0] - x[1])
         
+        total = 0
+        n = len(costs) // 2
+        # To optimize the company expenses,
+        # send the first n persons to the city A
+        # and the others to the city B
+        for i in range(n):
+            total += costs[i][0] + costs[i + n][1]
+        return total
