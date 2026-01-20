f42cf6285ca76ac5c47b8139a8094f89c76ca5db
yashladd <43058610+yashladd@users.noreply.github.com>
2026-01-20 11:11:16 -0800

    Time: 132 ms (24.37%), Space: 22.4 MB (11.69%) - LeetHub

--- 0676-implement-magic-dictionary/0676-implement-magic-dictionary.py
+++ 0676-implement-magic-dictionary/0676-implement-magic-dictionary.py
@@ -1,26 +1,14 @@
-class MagicDictionary:
+class MagicDictionary(object):
+    def _genneighbors(self, word):
+        for i in range(len(word)):
+            yield word[:i] + '*' + word[i+1:]
 
-    def __init__(self):
-        self.words = set()
+    def buildDict(self, words):
+        self.words = set(words)
+        self.count = collections.Counter(nei for word in words
+                                        for nei in self._genneighbors(word))
 
-    def buildDict(self, dictionary: List[str]) -> None:
-        self.words = set(dictionary)
-        
-
-    def search(self, searchWord: str) -> bool:
-        # print(self.words)
-        for i in range(len(searchWord)):
-            for ch_ord in range(ord('a'), ord('a') + 26):
-                if chr(ch_ord) != searchWord[i]:
-                    changedWord = searchWord[:i] + chr(ch_ord) + searchWord[i+1:]
-                    # print(changedWord, str(ch_ord))
-                    if changedWord in self.words:
-                        return True
-        return False
-        
-
-
-# Your MagicDictionary object will be instantiated and called as such:
-# obj = MagicDictionary()
-# obj.buildDict(dictionary)
-# param_2 = obj.search(searchWord)
+    def search(self, word):
+        return any(self.count[nei] > 1 or
+                   self.count[nei] == 1 and word not in self.words
+                   for nei in self._genneighbors(word))
