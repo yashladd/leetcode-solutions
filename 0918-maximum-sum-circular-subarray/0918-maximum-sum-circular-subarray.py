60bff412c318a0e29fc6d85fc989f3a462bc5316
yashladd <43058610+yashladd@users.noreply.github.com>
2026-01-21 08:58:16 -0800

    Time: 63 ms (69.92%), Space: 24.2 MB (17.51%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.py
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.py
@@ -4,15 +4,21 @@ class Solution:
         totalSum = sum(nums)
         maxSubSum = nums[0]
         currSum = nums[0]
-
+        # Maybe the ans in within the normal array not circular
+        # Find this max sub aray sum using kadane's algo
         for i in range(1, n):
             currSum = max(currSum + nums[i], nums[i])
             maxSubSum = max(currSum, maxSubSum)
-
-        print("Before", maxSubSum)
         
         currSum = nums[0]
         minSubSum = nums[0]
+
+        # [ * *  ### * * * ]
+        # If ### is minSum then total - sumof of this portion will give me the 
+        # max sum excluding this portion the sircular portion represented by *
+        # NOTE: only find minSum from [1, n-2] 
+        # if we find for all array then circular can be total - total = 0
+        # We want to take atleast 1 element in the subarry 
 
         for i in range(1, n-1):
             currSum = min(currSum + nums[i], nums[i])
