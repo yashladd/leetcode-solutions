674533cfc75fc62a50e05bdf2fe55ac5e3ca99e3
yashladd <43058610+yashladd@users.noreply.github.com>
2026-01-21 14:34:21 -0800

    Time: 93 ms (93.76%), Space: 78.7 MB (8.83%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,59 +1,65 @@
 class Node:
-    def __init__(self, key, val):
+    def __init__(self, key, val, next = None, prev = None):
         self.key = key
         self.val = val
-        self.prev = None
-        self.next = None
+        self.next = next
+        self.prev = prev
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.cap = capacity
-        self.head = Node(-1, -1)
-        self.tail = Node(-1, -1)
+        self.cache = {}
+        self.capacity = capacity
+        self.head = Node(-1,-1)
+        self.tail = Node(-1,-1)
         self.head.next = self.tail
         self.tail.prev = self.head
-        self.cache = {}
+
+
+    def deleteNode(self, node):
+        prevNode = node.prev
+        nextNode = node.next
+
+        prevNode.next = nextNode
+        nextNode.prev = prevNode
+
+        node.next = None
+        node.prev = None
+
+
+    def addFront(self, node):
+        secondLast = self.tail.prev
+        secondLast.next = node
+        node.prev = secondLast
+        node.next = self.tail
+        self.tail.prev = node
+        
 
     def get(self, key: int) -> int:
         if key not in self.cache:
             return -1
-
-        node = self.cache[key]
-        self.delete(node)
-        self.add(node)
-        return node.val
         
+        node = self.cache[key]
+        self.deleteNode(node)
+        self.addFront(node)
+        return node.val
+
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             node = self.cache[key]
-            del self.cache[key]
-            self.delete(node)
-        elif len(self.cache) == self.cap:
-            node = self.tail.prev
-            del self.cache[node.key]
-            self.delete(node)
+            node.val = value
+            self.deleteNode(node)
+            self.addFront(node)
+        else:
+            if len(self.cache) == self.capacity:
+                leastRecentlyUsed = self.head.next
+                self.deleteNode(leastRecentlyUsed)
+                del self.cache[leastRecentlyUsed.key]
 
-        node = Node(key, value)
-        self.cache[key] = node
-        self.add(node)
-
-    def delete(self, node):
-        p, n = node.prev, node.next
-        p.next = n
-        n.prev = p
-
-    def add(self, node):
-        n = self.head.next
-        self.head.next = node
-        node.prev = self.head
-        node.next = n
-        n.prev = node
-
-
-
-            
-
+            mostRecentlyUsed = Node(key, value)
+            self.cache[key] = mostRecentlyUsed
+            self.addFront(mostRecentlyUsed)
+        
         
 
 
