edfcb662056e317482398d357c57d7fed967ef5c
yashladd <43058610+yashladd@users.noreply.github.com>
2026-01-23 13:48:49 -0800

    Time: 0 ms (100%), Space: 19.4 MB (15.96%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.py
+++ 0140-word-break-ii/0140-word-break-ii.py
@@ -1,49 +1,27 @@
-class TrieNode:
-    def __init__(self):
-        self.children = {}
-        self.isWord = False
-
-class Trie:
-    def __init__(self):
-        self.root = TrieNode()
-
-    def addWord(self, word):
-        curr = self.root
-        for c in word:
-            if c not in curr.children:
-                curr.children[c] = TrieNode()
-            curr = curr.children[c]
-        curr.isWord = True
-
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
-        trie = Trie()
-        for word in wordDict:
-            trie.addWord(word)
-
         cache = {}
 
-        def backtrack(index):
-            if index == len(s):
-                return [""]
-            if index in cache:
-                return cache[index]
+        wd = set(wordDict)
 
+        def f(s):
             res = []
-            curr = trie.root
-            for i in range(index, len(s)):
-                char = s[i]
-                if char not in curr.children:
-                    break
-                curr = curr.children[char]
-                if curr.isWord:
-                    for suffix in backtrack(i + 1):
-                        if suffix:
-                            res.append(s[index:i + 1] + " " + suffix)
-                        else:
-                            res.append(s[index:i + 1])
+            if not s:
+                return []
+            if s in cache:
+                return cache[s]
+            for i in range(len(s)):
+                word = s[:i+1]
+                if word in wd:
+                    # CHECK: Is this the last word in the string?
+                    if i == len(s) - 1:
+                        res.append(word)  # Success! Add the word alone.
+                    else:
+                        suffs = f(s[i+1:])
+                        for suff in suffs:
+                            res.append(word + " " + suff) # Add word + space + suffix
 
-            cache[index] = res
+            cache[s] = res[:]
             return res
 
-        return backtrack(0)
+        return f(s)
