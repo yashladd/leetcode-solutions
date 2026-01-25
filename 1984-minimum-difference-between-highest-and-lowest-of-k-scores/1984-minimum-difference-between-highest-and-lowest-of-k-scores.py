cc357da085d52be1879e2fa5599e300582214ab7
yashladd <43058610+yashladd@users.noreply.github.com>
2026-01-24 16:36:02 -0800

    Time: 7 ms (76.95%), Space: 19.5 MB (15.85%) - LeetHub

--- 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.py
+++ 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.py
@@ -4,34 +4,16 @@ class Solution:
         [3 8 1 2 9 5 7] k = 3
         [1 2 3 5 7 8 9]
          0 1 2 3 4 5 6
-        1 1 2 3 4 4 4 4 5
         n = 7
         """
-        mini = float("inf")
-        h = Counter(nums)
-        sortedNumsWithCnt = list(sorted(h.items()))
-        print(sortedNumsWithCnt)
-        n = len(sortedNumsWithCnt)
-        for i, (num, cnt) in enumerate(sortedNumsWithCnt):
-            # if i >= len(sortedNumsWithCnt)-1:
-            #     con
-            print(i, num, cnt)
-            if cnt >= k:
-                mini = 0
-            else:
-                nextBig = float("inf")
-                remaining = k - cnt
-                # print(i , num , cnt, nextBig, remaining)
-                while remaining > 0 and i + 1 < n:
-                    remaining -= sortedNumsWithCnt[i+1][1]
-                    nextBig = sortedNumsWithCnt[i+1][0]
-                    i += 1
-                # print(i , num , cnt, nextBig, remaining)
-                if remaining <= 0:
-                    mini = min(mini, nextBig - num)
 
-        
-    
+        a = sorted(nums)
+
+        n = len(a)
+        mini = float("inf")
+        for i in range(n-k+1):
+            mini = min(mini, a[i + k -1] - a[i])
+
         return mini
 
         
