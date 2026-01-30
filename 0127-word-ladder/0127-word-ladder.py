46ef67a530122c8c70fd9637e099aa5821b6013e
yashladd <43058610+yashladd@users.noreply.github.com>
2026-01-30 07:50:04 -0800

    Time: 354 ms (24.77%), Space: 20.6 MB (58.45%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,19 +1,19 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        words = set(wordList)
+        wl = set(wordList)
         q = deque([(beginWord, 1)])
-        chrs = "abcdefghijklmnopqrstuvwxyz"
+        see = set()
+        see.add(beginWord)
         while q:
-            word, steps = q.popleft()
-            if word == endWord:
-                return steps
-            for i in range(len(word)):
-                for ch in chrs:
-                    trans = word[:i] + ch + word[i+1:]
-                    if trans in words:
-                        q.append((trans, steps + 1))
-                        words.discard(trans)
-                
+            w, d = q.popleft()
+            if w == endWord:
+                return d
+            for i in range(len(w)):
+                for ch in list(map(chr, range(ord('a'), ord('a') + 26))):
+                    t = w[:i] + ch + w[i+1:] 
+                    if t in wl and t not in see:
+                        q.append((t, d + 1))
+                        see.add(t)
+        
         return 0
-        
-        
+            
