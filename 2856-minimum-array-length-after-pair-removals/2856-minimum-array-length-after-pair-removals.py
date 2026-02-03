349db7456508faa050ed73828e24bb38ad69a65a
yashladd <43058610+yashladd@users.noreply.github.com>
2026-02-02 17:39:23 -0800

    Time: 4 ms (88.81%), Space: 32.2 MB (62.94%) - LeetHub

---
+++ 2856-minimum-array-length-after-pair-removals/2856-minimum-array-length-after-pair-removals.py
@@ -0,0 +1,9 @@
+class Solution:
+    """
+    Brilliant
+    """
+    def minLengthAfterRemovals(self, nums: List[int]) -> int:
+        n = len(nums)
+        i = nums[n // 2]
+        cnt = bisect_right(nums, i) - bisect_left(nums, i)
+        return max(n % 2, 2 * cnt - n)
