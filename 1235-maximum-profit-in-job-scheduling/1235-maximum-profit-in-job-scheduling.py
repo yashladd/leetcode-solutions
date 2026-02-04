da2ecc1283959e4a7767618c008d74bc0c7561b8
yashladd <43058610+yashladd@users.noreply.github.com>
2026-02-03 20:42:32 -0800

    Time: 89 ms (89.99%), Space: 33 MB (66.11%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
@@ -1,12 +1,23 @@
 class Solution:
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
         intervals = []
+        starts = []
         for s, e, p in zip(startTime, endTime, profit):
             intervals.append((s, e, p))
+            starts.append(s)
             
         intervals = sorted(intervals, key=lambda x: x[0])
+        starts.sort()
         n = len(intervals)
-        dp = [-1] * n
+        dp = [0] * (n + 1)
+        for index in range(n-1, -1, -1):
+            notTake = dp[index + 1]
+            take = intervals[index][2]
+            take += dp[bisect_left(starts, intervals[index][1])]
+            dp[index] = max(take, notTake)
+
+        return dp[0]
+
         
         def maximizePrice(index):
             if index >= n:
@@ -15,9 +26,9 @@ class Solution:
                 return dp[index]
             notTake = maximizePrice(index + 1)
             take = intervals[index][2]
-            nextIdx = index + 1
-            while nextIdx < n and intervals[nextIdx][0] < intervals[index][1]:
-                nextIdx += 1
+            ## USE BS TO FIND THE NEXT INDEX
+            nextIdx = bisect_left(starts, intervals[index][1])
+            
             take += maximizePrice(nextIdx)
             dp[index] = max(take, notTake)
             return dp[index]
