89d1b2ae1bfe1fe88557d99fedba8ad59b6522cc
yashladd <43058610+yashladd@users.noreply.github.com>
2026-02-06 09:39:56 -0800

    Time: 99 ms (62.75%), Space: 45.8 MB (9.9%) - LeetHub

--- 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
@@ -1,27 +1,19 @@
 class Solution:
     def getKth(self, lo: int, hi: int, k: int) -> int:
         h = []
+        from functools import cache
+
+        @cache
         def build(num):
-            q = deque([(num, 0)])
-            seen = set()
-            while q:
-                sz = len(q)
-                for _ in range(sz):
-                    num, step = q.popleft()
-                    if num == 1:
-                        return step
-                    
-                    if num%2:
-                        if num * 3 + 1 not in seen:
-                            seen.add(num * 3 + 1)
-                            q.append((num * 3 + 1, step + 1))
-                    else:
-                        if num // 2 not in seen:
-                            seen.add(num//2)
-                            q.append((num//2, step + 1))
+            if num == 1:
+                return 0
+            if num % 2 == 0:
+                return 1 + build(num // 2)
+            else:
+                return 1 + build(3 * num + 1)
 
         for x in range(lo, hi + 1):
-            # print(h, x, build(x))
+
             heappush(h, (-build(x), -x))
             if len(h) > k:
                 heappop(h)
