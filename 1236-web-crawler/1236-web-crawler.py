de07395f965ddeac744f615c282a659ba7b4064e
yashladd <43058610+yashladd@users.noreply.github.com>
2026-02-08 08:47:03 -0800

    Time: 174 ms (73.49%), Space: 25.7 MB (47.81%) - LeetHub

---
+++ 1236-web-crawler/1236-web-crawler.py
@@ -0,0 +1,23 @@
+class Solution:
+    def crawl(self, startUrl: str, htmlParser: 'HtmlParser') -> List[str]:
+        q = deque([startUrl])
+        vis = set([startUrl])
+
+        def get_hostname(u):
+            u = u.split("//")
+            return u[1].split("/")[0]
+
+        def same_hostname(u):
+            return get_hostname(u) == get_hostname(startUrl)
+
+        while q:
+            currUrl = q.popleft()
+
+            urls = htmlParser.getUrls(currUrl)
+
+            for u in urls:
+                if u not in vis and same_hostname(u):
+                    vis.add(u)
+                    q.append(u)
+
+        return list(vis)
