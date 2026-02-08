42bf28fcba7e8f2ea722f04e1a59ba966538ef2a
yashladd <43058610+yashladd@users.noreply.github.com>
2026-02-08 15:50:46 -0800

    Time: 839 ms (45.55%), Space: 25.7 MB (24.44%) - LeetHub

--- 1494-parallel-courses-ii/1494-parallel-courses-ii.py
+++ 1494-parallel-courses-ii/1494-parallel-courses-ii.py
@@ -1,35 +1,31 @@
 class Solution:
     def minNumberOfSemesters(self, n: int, relations: List[List[int]], k: int) -> int:
-        """
-
-        0000
-
-        """
         g = defaultdict(list)
-        indeg = [0 for _ in range(n)]
-
+        ind=[0 for _ in range(n)]
         for u, v in relations:
             g[u-1].append(v-1)
-            indeg[v-1] += 1
+            ind[v-1] += 1
 
         @cache
-        def rec(mask, indeg):
+        def dp(mask, deg):
             if not mask:
                 return 0
-
-            availCourses = [i for i in range(n) if mask & (1 << i) and not indeg[i]]
-            ans = float("inf")
-            for courses in combinations(availCourses, min(len(availCourses), k)):
-                newMask, newIndg = mask, list(indeg)
-                for node in courses:
-                    newMask ^= (1 << node)
-                    for nei in g[node]:
-                        newIndg[nei] -= 1
-                ans = min(ans, 1 + rec(newMask, tuple(newIndg)))
-
+            
+            availableCourses = []
+            # 11011
+            for i in range(n):
+                if mask & (1 << i) and deg[i] == 0:
+                    availableCourses.append(i)
+            ans = inf
+            for choice in combinations(availableCourses, min(len(availableCourses), k)):
+                newMask, newDeg = mask, list(deg)
+                for course in choice:
+                    newMask ^= (1 << course)
+                    for nei in g[course]:
+                        newDeg[nei] -= 1
+                ans = min(ans, 1 + dp(newMask, tuple(newDeg)))
             return ans
-            
-            
-        
-        return rec((1 << n) - 1, tuple(indeg))
 
+
+
+        return dp((1 << n) - 1, tuple(ind))
