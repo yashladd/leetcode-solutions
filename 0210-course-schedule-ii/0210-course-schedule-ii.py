8097311aaede7a834664978e9c2e2a9032bed87d
yashladd <43058610+yashladd@users.noreply.github.com>
2026-02-08 14:10:42 -0800

    Time: 6 ms (31.92%), Space: 20.6 MB (35.33%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,30 +1,25 @@
 class Solution:
-    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        adj = defaultdict(list)
-        for sr, de in prerequisites:
-            adj[de].append(sr)
-        stk = []
-        vis = [0] * numCourses
-        path = [0] * numCourses
+    def findOrder(self, numCourses: int, prereq: List[List[int]]) -> List[int]:
+        g = defaultdict(list)
+        ind = [0] * numCourses
 
-        def dfs(node):
-            vis[node] = 1
-            path[node] = 1
-            for nei in adj[node]:
-                if not vis[nei]:
-                    if dfs(nei): return True
-                elif path[nei]:
-                    return True
-            path[node] = 0  
-            stk.append(node)
-            return False
-        
-        for i in range(numCourses):
-            if not vis[i]:
-                if dfs(i): return []
-                
-        return stk[::-1]
-                
-                    
-            
-        
+        for u, v in prereq:
+            g[v].append(u)
+            ind[u] += 1
+        print(ind)
+        q = deque([i for i in range(numCourses) if ind[i] == 0])
+        topo = []
+        print(q)
+        while q:
+            node = q.popleft()
+            topo.append(node)
+            for nei in g[node]:
+                ind[nei] -= 1
+                if not ind[nei]:
+                    q.append(nei)
+
+        if len(topo) != numCourses:
+            return []
+
+        return topo
+
