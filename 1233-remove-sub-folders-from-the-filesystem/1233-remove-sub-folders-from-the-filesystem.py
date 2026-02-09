2767802bbd7f924e795c5ecfb91e755c5bb379e8
yashladd <43058610+yashladd@users.noreply.github.com>
2026-02-08 16:17:30 -0800

    Time: 155 ms (17.38%), Space: 41.2 MB (14.76%) - LeetHub

--- 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.py
+++ 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.py
@@ -1,30 +1,39 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end = False
+
 class Solution:
     def removeSubfolders(self, folder: List[str]) -> List[str]:
-        """
-        a bc bca cdf aaa aca azb zzo z
-
-
-
-
-        """
-
-        # folder = [x.split("/").lstrip().rstrip() for x in folder]
-
-        folder.sort()
-        res = [folder[0]]
-
-        print(folder)
-
-        index = 0
-
-        while index < len(folder):
-            nextIndex = index + 1
-            while nextIndex < len(folder) and folder[nextIndex].startswith(folder[index] + "/"):
-                nextIndex += 1
-
-            if nextIndex < len(folder):
-                res.append(folder[nextIndex])
-
-            index = nextIndex
-        return res
+        root = TrieNode()
         
+        # 1. Build Trie (O(N * L))
+        for path in folder:
+            node = root
+            for part in path.split('/')[1:]:
+                if part not in node.children:
+                    node.children[part] = TrieNode()
+                node = node.children[part]
+            node.is_end = True
+        
+        res = []
+        
+        # 2. Check each path against Trie (O(N * L))
+        for path in folder:
+            node = root
+            parts = path.split('/')[1:]
+            is_sub = False
+            
+            for i, part in enumerate(parts):
+                node = node.children[part]
+                # If we hit a node that is an end, AND it's not the node 
+                # for the current path itself (meaning it's a prefix),
+                # then 'path' is a sub-folder.
+                if node.is_end and i < len(parts) - 1:
+                    is_sub = True
+                    break
+            
+            if not is_sub:
+                res.append(path)
+                
+        return res
