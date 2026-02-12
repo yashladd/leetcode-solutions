c3d5b27b3db66a123d1ca6ee05cd045cd84e81c3
yashladd <43058610+yashladd@users.noreply.github.com>
2026-02-11 16:26:42 -0800

    Time: 366 ms (66.99%), Space: 19.8 MB (38.28%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
@@ -1,6 +1,23 @@
 class Solution:
-    def minDifficulty(self, jd: List[int], d: int) -> int:
+    def minDifficulty(self, jd: List[int], da: int) -> int:
         N = len(jd)
+
+        dp = [[inf] * (da+1) for _ in range(N+1)]
+        dp[N][0] = 0
+
+        for i in range(N-1,-1,-1):
+            for d in range(da+1):
+                costToEndHere = jd[i] + dp[i+1][d-1]
+                maxi = -inf
+                for j in range(i, N):
+                    maxi = max(maxi, jd[j])
+                    costToEndHere = min(costToEndHere, maxi + dp[j+1][d-1])
+                
+                dp[i][d] = costToEndHere
+        res = dp[0][da]  
+        return res if res != inf else -1
+
+
         @cache
         def f(i, d):
             if i == N:
