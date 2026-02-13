750c050d17ac8149a26969d844c1f4078a7a5aad
yashladd <43058610+yashladd@users.noreply.github.com>
2026-02-13 08:45:54 -0800

    Time: 0 ms (100%), Space: 21.1 MB (31.05%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        xor = 0
+
+        for n in nums: xor ^= n
+
+
+        diff_bit = 1
+
+        while not (xor & diff_bit):
+            diff_bit = (diff_bit << 1)
+
+        a, b = 0,0 
+
+        for n in nums:
+            if diff_bit & n:
+                a ^= n
+            else:
+                b ^= n
+
+        return [a,b]
