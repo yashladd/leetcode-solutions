7f24a464d4776eed63147e5327fb180a2f604309
yashladd <43058610+yashladd@users.noreply.github.com>
2026-03-01 12:53:17 -0800

    Time: 109 ms (76.75%), Space: 76.7 MB (93.92%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,66 +1,27 @@
-class Node:
-    def __init__(self, key, val, next = None, prev = None):
-        self.key = key
-        self.val = val
-        self.next = next
-        self.prev = prev
-
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.cache = {}
         self.capacity = capacity
-        self.head = Node(-1,-1)
-        self.tail = Node(-1,-1)
-        self.head.next = self.tail
-        self.tail.prev = self.head
+        self.cache = OrderedDict()
 
-
-    def deleteNode(self, node):
-        prevNode = node.prev
-        nextNode = node.next
-
-        prevNode.next = nextNode
-        nextNode.prev = prevNode
-
-        node.next = None
-        node.prev = None
-
-
-    def addFront(self, node):
-        secondLast = self.tail.prev
-        secondLast.next = node
-        node.prev = secondLast
-        node.next = self.tail
-        self.tail.prev = node
-        
 
     def get(self, key: int) -> int:
         if key not in self.cache:
             return -1
+        self.cache.move_to_end(key)
+        return self.cache[key]
         
-        node = self.cache[key]
-        self.deleteNode(node)
-        self.addFront(node)
-        return node.val
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            node = self.cache[key]
-            node.val = value
-            self.deleteNode(node)
-            self.addFront(node)
+            self.cache.move_to_end(key)
+            self.cache[key] = value
         else:
             if len(self.cache) == self.capacity:
-                leastRecentlyUsed = self.head.next
-                self.deleteNode(leastRecentlyUsed)
-                del self.cache[leastRecentlyUsed.key]
+                self.cache.popitem(last = False)
+            self.cache[key] = value
 
-            mostRecentlyUsed = Node(key, value)
-            self.cache[key] = mostRecentlyUsed
-            self.addFront(mostRecentlyUsed)
-        
-        
+            
 
 
 # Your LRUCache object will be instantiated and called as such:
