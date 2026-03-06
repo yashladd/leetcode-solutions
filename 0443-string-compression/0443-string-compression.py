458dc1a1de2e7745727f3ca7774cd3d07d61dfc8
yashladd <43058610+yashladd@users.noreply.github.com>
2026-03-05 16:48:42 -0800

    Time: 0 ms (100%), Space: 19.5 MB (53.99%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -1,31 +1,21 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        n = len(chars)
-        charIdx = 0
-        pos = 0
+        char_idx = 0
+        N = len(chars)
+        ptr = 0
+        while char_idx < N:
+            curr_char = chars[char_idx]
+            curr_len = 1
+            while char_idx + 1 < N and chars[char_idx + 1] == curr_char:
+                char_idx += 1
+                curr_len += 1
 
-        while charIdx < n:
-            currChar = chars[charIdx]
-            nextCharIdx = charIdx + 1
-            charCnt = 1
-            while nextCharIdx < n and chars[nextCharIdx] == currChar:
-                nextCharIdx += 1
-                charCnt += 1
-            charIdx = nextCharIdx
-            chars[pos] = currChar
-            pos += 1
-            if charCnt == 1:
-                continue
-            for ch in str(int(charCnt)):
-                chars[pos] = ch
-                pos += 1
-        
-        return pos
+            chars[ptr] = curr_char
+            ptr += 1
+            if curr_len > 1:
+                for ch in str(curr_len):
+                    chars[ptr] = ch
+                    ptr += 1
+            char_idx += 1
 
-
-
-
-
-
-
-        
+        return ptr
