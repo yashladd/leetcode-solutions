02b92bdbb4fb37402ddc9f46bbc54aec8a2489c8
yashladd <43058610+yashladd@users.noreply.github.com>
2026-03-30 07:04:07 -0700

    Time: 35 ms (60.37%), Space: 20.5 MB (47.87%) - LeetHub

--- 2086-minimum-number-of-food-buckets-to-feed-the-hamsters/2086-minimum-number-of-food-buckets-to-feed-the-hamsters.py
+++ 2086-minimum-number-of-food-buckets-to-feed-the-hamsters/2086-minimum-number-of-food-buckets-to-feed-the-hamsters.py
@@ -1,25 +1,22 @@
 class Solution:
     def minimumBuckets(self, hamsters: str) -> int:
-        n = len(hamsters)
-        hamsters = list(hamsters) # Convert to list to "place" buckets
-        buckets = 0
-        
-        for i in range(n):
-            if hamsters[i] == 'H':
-                # 1. Check if already fed by a bucket at i-1
-                if i > 0 and hamsters[i-1] == 'B':
+        positions = list(hamsters)
+        # print(positions)
+
+        num_buckets = 0
+
+        for i, pos in enumerate(positions):
+            if pos == "H":
+                if i > 0 and positions[i-1] == "B":
                     continue
-                
-                # 2. Try placing a bucket at i+1 (Greedy choice)
-                if i + 1 < n and hamsters[i+1] == '.':
-                    buckets += 1
-                    hamsters[i+1] = 'B' # Mark as bucket
-                # 3. Try placing a bucket at i-1
-                elif i > 0 and hamsters[i-1] == '.':
-                    buckets += 1
-                    # No need to mark hamsters[i-1] as 'B' since we already passed it
-                # 4. No possible placement
+
+                if i + 1 < len(hamsters) and positions[i+1] == ".":
+                    positions[i+1] = "B"
+                    num_buckets += 1
+                elif i - 1 >= 0 and positions[i-1] == ".":
+                    positions[i-1] = "B"
+                    num_buckets += 1
                 else:
                     return -1
-                    
-        return buckets
+
+        return num_buckets
