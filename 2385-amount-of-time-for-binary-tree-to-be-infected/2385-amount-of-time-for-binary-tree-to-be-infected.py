c345b5a41656852615f440331c57c41c1df21ac1
yashladd <43058610+yashladd@users.noreply.github.com>
2026-07-27 16:55:11 -0700

    Time: 182 ms (39.98%), Space: 111.6 MB (5.05%) - LeetHub

---
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.py
@@ -0,0 +1,44 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def amountOfTime(self, root: Optional[TreeNode], start: int) -> int:
+        g = defaultdict(list)
+
+        vis = set()
+        def build(node, par):
+            vis.add(node)
+            if not node:
+                return 
+
+            if node and par:
+                g[node.val].append(par.val)
+                g[par.val].append(node.val)
+
+            if node.left:
+                build(node.left, node)
+            
+            if node.right:
+                build(node.right, node)
+
+        build(root, None)
+
+
+        level = [start]
+        vis = {start}
+        time = 0
+        while level:
+            next_level = []
+            for node in level:
+                for ch in g[node]:
+                    if ch not in vis:
+                        vis.add(ch)
+                        next_level.append(ch)
+            level = next_level
+            time += 1
+
+
+        return time - 1
