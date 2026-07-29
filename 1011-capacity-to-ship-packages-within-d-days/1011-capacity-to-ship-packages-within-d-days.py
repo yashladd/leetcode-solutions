caacb57d012325da1c9b442f94e7bd06e69a732a
yashladd <43058610+yashladd@users.noreply.github.com>
2026-07-29 06:26:27 -0700

    Time: 300 ms (15.16%), Space: 23.9 MB (65.87%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -1,21 +1,37 @@
 class Solution:
-    def shipWithinDays(self, a: List[int], threshold: int) -> int:
-        l, r = max(a), sum(a)
-        def canMake(mid):
-            days, wt = 1, 0
-            for n in a:
-                if wt + n <= mid:
-                    wt += n
-                else:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        low = min(weights)
+        high = sum(weights)
+
+        min_capacity = high
+
+
+        def can_ship(capacity, max_days):
+            days = 1
+            curr_load = 0
+            for i, weight in enumerate(weights):
+                if weight > capacity:
+                    return False
+                if days > max_days:
+                    return False
+                curr_load += weight
+                if curr_load > capacity:
+                    curr_load = weight
                     days += 1
-                    wt = n
-            return days <= threshold
-        ans = r
-        while l <= r:
-            mid = (l + r) >> 1
-            if canMake(mid):
-                ans = min(ans, mid)
-                r = mid - 1
+                    
+            return days <= max_days
+                
+
+        while low <= high:
+            mid = (low + high) >> 1
+
+            if can_ship(mid, days):
+                min_capacity = mid
+                high = mid - 1
             else:
-                l = mid + 1
-        return ans
+                low = mid + 1
+
+
+        return min_capacity
+
+
