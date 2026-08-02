fdbdc0e50a11dab9b8b7f68c257ca80537c07260
yashladd <43058610+yashladd@users.noreply.github.com>
2026-08-02 14:45:05 -0700

    Time: 1028 ms (18.89%), Space: 80 MB (5.05%) - LeetHub

--- 3413-maximum-coins-from-k-consecutive-bags/3413-maximum-coins-from-k-consecutive-bags.py
+++ 3413-maximum-coins-from-k-consecutive-bags/3413-maximum-coins-from-k-consecutive-bags.py
@@ -1,32 +1,40 @@
 class Solution:
     def maximumCoins(self, coins: List[List[int]], k: int) -> int:
+
+        
+
+        def max_amount_left_aligned(coins):
+            coins = sorted(coins)
+            N = len(coins)
+
+            pref_amount = [0] * (N+1)
+
+            for i, (l, r, c) in enumerate(coins):
+                pref_amount[i+1] = pref_amount[i] + (r - l + 1) * c
+
+            interval_starts = [l for l, _, _  in coins]
+            best = 0
+            for i, (l, r, c) in enumerate(coins):
+                start = l
+                end = l + k - 1
+
+                j = bisect_right(interval_starts, end) - 1
+
+                l_j, r_j, c_j = coins[j]
+
+                amount = pref_amount[j] - pref_amount[i]
+
+                partial_amount = (min(end, r_j) - l_j  + 1) * c_j
+
+                amount += partial_amount
+
+                best = max(best, amount)
+
+            return best
+
+
         return max(
-            self._best_left_aligned(coins, k),
-            self._best_left_aligned([[-r, -l, c] for l, r, c in coins], k)
+            max_amount_left_aligned(coins),
+            max_amount_left_aligned([(-r, -l, c) for l, r, c in coins])
         )
 
-    def _best_left_aligned(self, segs, k):
-        segs = sorted(segs)
-        N = len(segs)
-        pref = [0] * (N+1)
-        starts = [l for l, r, c in segs]
-
-        for i, (l, r, c) in enumerate(segs):
-            pref[i+1] = pref[i] + (r-l+1) * c
-
-        best = 0
-
-        for i, (l, r, c) in enumerate(segs):
-            start = l
-            end = l + k - 1
-
-            j = bisect_right(starts, end) - 1
-
-            curr_total = pref[j] - pref[i]
-            l_j, r_j, c_j = segs[j]
-            clipped_segment = min(r_j, end) - l_j + 1
-            clipped_amount = clipped_segment * c_j
-
-            best = max(best, curr_total + clipped_amount)
-
-        return best
