e82e427cb2360db24a52505a40e33f9c75ce1142
yashladd <43058610+yashladd@users.noreply.github.com>
2026-08-02 14:00:18 -0700

    Time: 759 ms (34.41%), Space: 41.8 MB (82.55%) - LeetHub

--- 0815-bus-routes/0815-bus-routes.py
+++ 0815-bus-routes/0815-bus-routes.py
@@ -1,32 +1,38 @@
 class Solution:
     def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
-        N = len(routes)
-        g = defaultdict(list)
-        if source == target: return 0
+        if source == target:
+            return 0
+        stop_to_buses = defaultdict(list)
 
-        for bus, stops in enumerate(routes):
+
+        for i, stops in enumerate(routes):
             for stop in stops:
-                g[stop].append(bus)
+                stop_to_buses[stop].append(i)
 
         vis = set()
         q = deque()
-        for bus in g[source]:
-            q.append((bus, 1))
+        for bus in stop_to_buses[source]:
+            q.append(bus)
             vis.add(bus)
 
-        while q:
-            s = len(q)
-            for _ in range(s):
-                bus, hops = q.popleft()
-                for stop in routes[bus]:
-                    if stop == target:
-                        return hops
+        num_buses = 1
 
-                    for bus in g[stop]:
+        while q:
+            sz = len(q)
+
+            for _ in range(sz):
+                bus_no = q.popleft()
+
+                for stop in routes[bus_no]:
+                    if stop == target:
+                        return num_buses
+
+                    for bus in stop_to_buses[stop]:
                         if bus not in vis:
-                            q.append((bus, hops + 1))
+                            q.append(bus)
                             vis.add(bus)
 
+            num_buses += 1
+
         return -1
-
-
+                
