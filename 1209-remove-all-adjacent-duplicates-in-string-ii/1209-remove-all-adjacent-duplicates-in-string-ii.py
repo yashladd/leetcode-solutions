524f19538500ddacc0c285958414cf86abe5a6bc
yashladd <43058610+yashladd@users.noreply.github.com>
2026-08-03 12:33:23 -0700

    Time: 76 ms (5.74%), Space: 19.8 MB (99.7%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
@@ -7,16 +7,13 @@ class Solution:
 k = 2
         """
         for ch in s:
-            if string_pref:
-                if string_pref[-1][0] == ch:
-                    if len(string_pref[-1]) + 1 >= k:
-                        string_pref.pop()
-                    else:
-                        string_pref[-1] += ch
-                else:
-                    string_pref.append(ch)
+            if string_pref and string_pref[-1][-1] == ch:
+                string_pref[-1] += ch
             else:
                 string_pref.append(ch)
+
+            if string_pref and len(string_pref[-1]) == k:
+                string_pref.pop()
 
         return "".join(string_pref)
 
