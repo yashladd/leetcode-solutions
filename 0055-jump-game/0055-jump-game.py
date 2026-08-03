4a363159c27e26752a0a4b1fb075ca1b8452c41f
yashladd <43058610+yashladd@users.noreply.github.com>
2026-08-03 14:25:16 -0700

    Time: 15 ms (78.42%), Space: 20.3 MB (62.64%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,11 +1,13 @@
 class Solution:
-    def canJump(self, nums):
-        N = len(nums)
-        maxIdx = 0
-        for i in range(N-1):
-            if maxIdx < i:
-                return False
-            maxIdx = max(maxIdx, i + nums[i])
-            if maxIdx >= N-1:
+    def canJump(self, nums: List[int]) -> bool:
+        n = len(nums)
+        if n == 1: return True
+        farthestIdx = 0
+
+        for i, j in enumerate(nums):
+            if i > farthestIdx: return False 
+            if farthestIdx >= n:
                 return True
-        return maxIdx >= N-1
+            farthestIdx = max(farthestIdx, j + i)
+
+        return farthestIdx >= n-1
