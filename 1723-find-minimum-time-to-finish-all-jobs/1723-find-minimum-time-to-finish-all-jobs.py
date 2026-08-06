4db2f96bfa8a57944f543b5138dd936096c8eb48
yashladd <43058610+yashladd@users.noreply.github.com>
2026-08-06 16:50:09 -0700

    Time: 2521 ms (33.51%), Space: 19.4 MB (48.55%) - LeetHub

--- 1723-find-minimum-time-to-finish-all-jobs/1723-find-minimum-time-to-finish-all-jobs.py
+++ 1723-find-minimum-time-to-finish-all-jobs/1723-find-minimum-time-to-finish-all-jobs.py
@@ -1,31 +1,29 @@
 class Solution:
     def minimumTimeRequired(self, jobs: List[int], k: int) -> int:
-        # Sort descending to fail fast
-        jobs.sort(reverse=True)
-        worker_loads = [0] * k
-        self.res = sum(jobs) # Initial upper bound
+        N = len(jobs)
+        jobs.sort(reverse = True)
+        min_max_load = sum(jobs)
+        worker_load = [0] * k
+        def back(job_idx):
+            nonlocal min_max_load
+            if  job_idx == N:
+                min_max_load = min(min_max_load, max(worker_load))
+                return 
 
-        def backtrack(job_idx):
-            # Base case: All jobs assigned
-            if job_idx == len(jobs):
-                self.res = min(self.res, max(worker_loads))
-                return
 
-            for i in range(k):
-                # Pruning: If this path is already worse than our best, skip
-                if worker_loads[i] + jobs[job_idx] >= self.res:
+            for worker_id in range(k):
+                if worker_load[worker_id] + jobs[job_idx] >= min_max_load:
                     continue
-                
-                # Assign job
-                worker_loads[i] += jobs[job_idx]
-                backtrack(job_idx + 1)
-                # Backtrack
-                worker_loads[i] -= jobs[job_idx]
 
-                # Optimization: If worker was empty, don't try next empty workers
-                # This avoids redundant permutations.
-                if worker_loads[i] == 0:
+                worker_load[worker_id] += jobs[job_idx]
+                back(job_idx + 1)
+                worker_load[worker_id] -= jobs[job_idx]
+
+                if worker_load[worker_id] == 0:
                     break
 
-        backtrack(0)
-        return self.res
+        back(0)
+        return min_max_load
+
+
+
