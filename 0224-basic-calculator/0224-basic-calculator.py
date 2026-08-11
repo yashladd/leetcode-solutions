06f8b326bb48f40d2fa16594d79f1a85358f0785
yashladd <43058610+yashladd@users.noreply.github.com>
2026-08-11 15:04:55 -0700

    Time: 71 ms (20.78%), Space: 21.8 MB (9.85%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.py
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -1,28 +1,44 @@
 class Solution:
-    def calculate(self, s):    
-        def calc(it):
-            def update(op, v):
-                if op == "+": stack.append(v)
-                if op == "-": stack.append(-v)
-                if op == "*": stack.append(stack.pop() * v)
-                if op == "/": stack.append(int(stack.pop() / v))
-        
-            num, stack, sign = 0, [], "+"
-            
-            while it < len(s):
-                if s[it].isdigit():
-                    num = num * 10 + int(s[it])
-                elif s[it] in "+-*/":
-                    update(sign, num)
-                    num, sign = 0, s[it]
-                elif s[it] == "(":
-                    num, j = calc(it + 1)
-                    it = j - 1
-                elif s[it] == ")":
-                    update(sign, num)
-                    return sum(stack), it + 1
-                it += 1
-            update(sign, num)
-            return sum(stack)
+    operators = "+-*/"
 
-        return calc(0)
+    def calculate(self, s: str) -> int:
+        state = {"s": s, "it": 0}   # mutable cursor shared across recursive calls
+        value, _ = self._calc(state)
+        return value
+
+    def _update(self, op: str, v: int, stack: list) -> None:
+        if op == "+":
+            stack.append(v)
+        elif op == "-":
+            stack.append(-v)
+        elif op == "*":
+            stack.append(stack.pop() * v)
+        elif op == "/":
+            # int(a / b) truncates toward zero (python // floors, wrong for negatives)
+            stack.append(int(stack.pop() / v))
+
+    def _calc(self, state: dict):
+        s = state["s"]
+        num, stack, sign = 0, [], "+"
+        it = state["it"]
+
+        while it < len(s):
+            ch = s[it]
+            if ch.isdigit():
+                num = num * 10 + int(ch)
+            elif ch in self.operators:
+                self._update(sign, num, stack)
+                num, sign = 0, ch
+            elif ch == "(":
+                state["it"] = it + 1
+                num, j = self._calc(state)
+                it = j
+            elif ch == ")":
+                self._update(sign, num, stack)
+                return sum(stack), it
+            # spaces fall through, no-op
+            it += 1
+
+        state["it"] = it
+        self._update(sign, num, stack)   # flush the trailing operand
+        return sum(stack), it
