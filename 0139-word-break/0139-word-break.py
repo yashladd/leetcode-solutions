81152c884fb22a50d5062a59b6c1503ec41f9e78
yashladd <43058610+yashladd@users.noreply.github.com>
2026-08-13 13:05:26 -0700

    Time: 7 ms (21.59%), Space: 19.4 MB (52.75%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,22 +1,26 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        n = len(s)
-        dp = [-1]*n
-        def f(i):
-            if i == n:
-                return True
-            if dp[i] != -1:
-                return dp[i]
-            
-            j = i
-            while j < n:
-                if s[i:j+1] in wordDict:
-                    if f(j+1):
-                        dp[i] = True
-                        return True
-                j += 1
-            dp[i] = False
-            return False
-        
-        
-        return f(0)
+        vocab = set(wordDict)
+        cache = {}
+        return self._can_break(0, s, vocab, cache)
+
+
+    def _can_break(self, idx: int, s: str, vocab: set[str], cache: dict[int, bool]):
+        if idx >= len(s):
+            return True
+
+        if idx in cache:
+            return cache[idx]
+
+        for break_idx in range(idx+1, len(s)+1):
+            broken_word = s[idx: break_idx]
+            if broken_word in vocab:
+                is_success =  self._can_break(break_idx, s, vocab, cache)
+                if is_success:
+                    cache[idx] = True
+                    return True
+        cache[idx] = False
+        return False
+
+
+
