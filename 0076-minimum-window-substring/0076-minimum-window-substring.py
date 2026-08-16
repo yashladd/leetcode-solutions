5fa54546daa68203069f670e4f201c6f9e88a6be
yashladd <43058610+yashladd@users.noreply.github.com>
2026-08-15 19:06:46 -0700

    Time: 147 ms (20.15%), Space: 19.8 MB (11.35%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,29 +1,37 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        tF = Counter(t)
-        sF = Counter()
-        i, j = 0, 0
-        n = len(s)
-        res = ""
-        mini = float("inf")
+        tf = Counter(t)
+
+        req = len(tf)
+
+        mat = 0
+
+        sf = Counter()
+
+        l = 0
+        min_l = inf
+        min_s = ""
+        for r, ch in enumerate(s):
+            sf[ch] += 1
+            if sf[ch] == tf[ch]:
+                mat += 1
+
+            while req == mat and l <= r:
+                if (r-l + 1) < min_l:
+                    min_l = r - l  +1
+                    min_s = s[l:r+1]
+
+                sf[s[l]] -= 1
+                if sf[s[l]] < tf[s[l]]:
+                    mat -= 1
+
+                if not sf[s[l]]:
+                    del sf[s[l]]
+
+                l += 1
+
+        return min_s
+
+
+
         
-        
-        def check():
-            for k, cnt in tF.items():
-                if sF[k] < cnt:
-                    return False
-            return True
-        
-        while j < n:
-            sF[s[j]] += 1
-            while check():
-                if j-i + 1 < mini:
-                    mini = j - i + 1
-                    res = s[i:j+1]
-                sF[s[i]] -= 1
-                i += 1
-            j += 1
-            
-        return res
-                
-                
