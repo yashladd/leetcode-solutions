c1c57e9e83e54ace5bc422fd9a40fc99c5cf645a
yashladd <43058610+yashladd@users.noreply.github.com>
2026-08-15 19:07:08 -0700

    Time: 147 ms (20.15%), Space: 19.8 MB (11.35%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,37 +1,34 @@
+from collections import Counter
+from math import inf
+
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
+        if not t or not s:
+            return ""
+        
         tf = Counter(t)
-
-        req = len(tf)
-
+        req = len(tf)  # Number of unique characters in t to match
         mat = 0
-
         sf = Counter()
-
+        
         l = 0
         min_l = inf
         min_s = ""
+        
         for r, ch in enumerate(s):
             sf[ch] += 1
-            if sf[ch] == tf[ch]:
+            if ch in tf and sf[ch] == tf[ch]:
                 mat += 1
-
-            while req == mat and l <= r:
-                if (r-l + 1) < min_l:
-                    min_l = r - l  +1
-                    min_s = s[l:r+1]
-
-                sf[s[l]] -= 1
-                if sf[s[l]] < tf[s[l]]:
+                
+            while mat == req:
+                if (r - l + 1) < min_l:
+                    min_l = r - l + 1
+                    min_s = s[l:r + 1]
+                
+                left_ch = s[l]
+                sf[left_ch] -= 1
+                if left_ch in tf and sf[left_ch] < tf[left_ch]:
                     mat -= 1
-
-                if not sf[s[l]]:
-                    del sf[s[l]]
-
                 l += 1
-
+                
         return min_s
-
-
-
-        
