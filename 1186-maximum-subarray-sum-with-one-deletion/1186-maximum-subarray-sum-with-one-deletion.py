0acbcb502ff90a64789fc1e95df93db208bf2f17
yashladd <43058610+yashladd@users.noreply.github.com>
2026-09-25 09:32:51 +0530

    Time: 15 ms (94.78%), Space: 27.7 MB (81.08%) - LeetHub

--- 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.py
+++ 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.py
@@ -1,25 +1,11 @@
 class Solution:
     def maximumSum(self, arr: list[int]) -> int:
-        n = len(arr)
-        L = [0] * n
+        keep = best = arr[0]
+        drop = -inf
 
-        L[0] = arr[0]
-
-        for i in range(1, n):
-            L[i] = max(arr[i], L[i-1] + arr[i])
-
-
-        R  = [0] * n
-        R[-1] = arr[-1]
-
-        for i in range(n-2, -1, -1):
-            R[i] = max(arr[i], R[i+1] + arr[i])
-
-
-        best = max(L)
-
-
-        for i in range(1,n-1):
-            best = max(best, L[i-1] + R[i+1])
+        for x in arr[1:]:
+            drop = max(keep, drop + x)
+            keep = max(keep + x, x)
+            best = max(best, keep, drop)
 
         return best
