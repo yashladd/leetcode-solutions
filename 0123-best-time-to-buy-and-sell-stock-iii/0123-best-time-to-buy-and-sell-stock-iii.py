1b4cb81de5fe41810b558d9c8ac4c4d18e169283
yashladd <43058610+yashladd@users.noreply.github.com>
2026-09-26 17:42:18 +0530

    Time: 2141 ms (5.01%), Space: 709.8 MB (5.03%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+
+        @cache
+        def f(i, b, t):
+            if t == 0:
+                return 0
+
+            if i == len(prices):
+                return 0
+
+            if b:
+                return max(
+                    -prices[i] + f(i, 0, t),
+                    f(i+1, 1, t)
+                )
+            
+            return max(
+                prices[i] + f(i, 1, t-1),
+                f(i+1, 0, t)
+            )
+
+        return f(0, 1, 2)
+        
